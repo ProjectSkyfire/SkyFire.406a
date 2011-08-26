1841f692813ea1a70e7c285663f5d33547485222
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-26 00:29:41 -0500

    SQL: fixed stupid mistake...

--- sql/updates/auth/2011_08_26_1_auth_realmlist.sql
+++ sql/updates/auth/2011_08_26_1_auth_realmlist.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `realmlist` CHANGE `gamebuild` `gamebuild` INT( 11 ) UNSIGNED NOT NULL DEFAULT '14333';
+ALTER TABLE `realmlist` CHANGE `gamebuild` `gamebuild` INT( 11 ) UNSIGNED NOT NULL DEFAULT '13623';
 UPDATE `realmlist` SET `gamebuild`=13623;
