5bfaadcec900a069a152c355646068c025101f7e
Bootz <Stage6Dev@EMPulseGaming.com>
2011-09-02 15:23:18 -0500

    Core/DBC: re-added a removed assertion from fileloader

--- src/server/game/DataStores/DBCStores.cpp
+++ src/server/game/DataStores/DBCStores.cpp
@@ -209,7 +209,7 @@ template<class T>
 inline void LoadDBC(uint32& availableDbcLocales, StoreProblemList& errlist, DBCStorage<T>& storage, const std::string& dbc_path, const std::string& filename, const std::string * custom_entries = NULL, const std::string * idname = NULL)
 {
 	// compatibility format and C++ structure sizes
-	if (!(DBCFileLoader::GetFormatRecordSize(storage.GetFormat()) == sizeof(T) || LoadDBC_assert_print(DBCFileLoader::GetFormatRecordSize(storage.GetFormat()), sizeof(T), filename)))
+    ASSERT(DBCFileLoader::GetFormatRecordSize(storage.GetFormat()) == sizeof(T) || LoadDBC_assert_print(DBCFileLoader::GetFormatRecordSize(storage.GetFormat()), sizeof(T), filename)))
 
 	++DBCFileCount;
 	std::string dbc_filename = dbc_path + filename;
