b1f5a1bd8a6b57f7608bea187b1118a598200d9c
Bootz <Stage6Dev@EMPulseGaming.com>
2011-09-12 02:21:43 -0500

    Scripts/BWD: Fixed the use of MAX_ENCOUNTERS.
    hells bells that was a stupid fix. lol
    
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- src/server/scripts/EasternKingdoms/BlackwingDescent/blackwing_descent.h
+++ src/server/scripts/EasternKingdoms/BlackwingDescent/blackwing_descent.h
@@ -21,43 +21,44 @@
 
 enum SharedSpells
 {
-    SPELL_BERSERK1 = 26662, 
-    SPELL_BERSERK2 = 64238, 
+    SPELL_BERSERK1 = 26662,
+    SPELL_BERSERK2 = 64238,
 };
 
 enum Data
 {
     //Encounters
-    DATA_MAGMAW                  = 0, 
-    DATA_OMNOTRON_DEFENSE_SYSTEM = 1, 
-    DATA_MALORIAK                = 2, 
-    DATA_ATRAMEDES               = 3, 
-    DATA_CHIMAERON               = 4, 
-    DATA_NEFARIAN                = 5, 
-        
+    DATA_MAGMAW                  = 0,
+    DATA_OMNOTRON_DEFENSE_SYSTEM = 1,
+    DATA_MALORIAK                = 2,
+    DATA_ATRAMEDES               = 3,
+    DATA_CHIMAERON               = 4,
+    DATA_NEFARIAN                = 5,
+
     //Additional Entities
-    DATA_ARCANOTRON_GUID         = 6, 
-    DATA_ELECTRON_GUID           = 7, 
-    DATA_MAGMATRON_GUID          = 8, 
-    DATA_TOXITRON_GUID           = 9, 
-    DATA_ONYXIA_GUID             = 10, 
-    DATA_BILE_O_TRON_800         = 11, 
+    DATA_ARCANOTRON_GUID         = 6,
+    DATA_ELECTRON_GUID           = 7,
+    DATA_MAGMATRON_GUID          = 8,
+    DATA_TOXITRON_GUID           = 9,
+    DATA_ONYXIA_GUID             = 10,
+    DATA_BILE_O_TRON_800         = 11,
+    MAX_ENCOUNTER                = 12
 };
 
 enum Creatures
 {
-    NPC_MAGMAW               = 41570, 
-    NPC_ARCANOTRON           = 42166, 
-    NPC_ELECTRON             = 42179, 
-    NPC_MAGMATRON            = 42178, 
-    NPC_TOXITRON             = 42180, 
-    NPC_MALORIAK             = 41378, 
-    NPC_ATRAMEDES            = 41442, 
-    NPC_CHIMAERON            = 43296, 
-    NPC_BILE_O_TRON_800      = 44418, 
-    NPC_NEFARIAN             = 41376, 
-    NPC_ONYXIA               = 41270, 
-    NPC_LORD_VICTOR_NEFARIAN = 41379, 
+    NPC_MAGMAW               = 41570,
+    NPC_ARCANOTRON           = 42166,
+    NPC_ELECTRON             = 42179,
+    NPC_MAGMATRON            = 42178,
+    NPC_TOXITRON             = 42180,
+    NPC_MALORIAK             = 41378,
+    NPC_ATRAMEDES            = 41442,
+    NPC_CHIMAERON            = 43296,
+    NPC_BILE_O_TRON_800      = 44418,
+    NPC_NEFARIAN             = 41376,
+    NPC_ONYXIA               = 41270,
+    NPC_LORD_VICTOR_NEFARIAN = 41379,
 };
 
 #endif
