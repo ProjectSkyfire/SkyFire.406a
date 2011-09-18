336de450042c5d07c9e9ad94e6b648c1e8705d7f
Bootz <Stage6Dev@EMPulseGaming.com>
2011-09-18 15:22:03 -0500

    Core/Spells: Fixed Translocation orbs to Silvermoon and Undercity.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4347,6 +4347,12 @@ void Spell::EffectScriptEffect(SpellEffIndex effIndex)
                     }
                     return;
                 }
+                case 35376:
+                   m_caster->TeleportTo(0, 1805.99f, 341.32f, 70.66f, 1.6f);
+                   break;
+                case 35727:
+                   m_caster->TeleportTo(530, 10038.7f, -7000.9f, 61.86f, 3.05f);
+                   break;
                 case 45204: // Clone Me!
                     m_caster->CastSpell(unitTarget, damage, true);
                     break;
