041a9b33d0f2612593a2c7b9e31c599573dd8784
Bootz <Stage6Dev@EMPulseGaming.com>
2011-09-21 06:15:11 -0500

    Core/Spell: Fix spell related to reputation in Oracle and Frenzy faction
    thanks to: Kiper

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -1313,50 +1313,49 @@ class spell_gen_vehicle_scaling : public SpellScriptLoader
         }
 };
 
-
 class spell_gen_oracle_wolvar_reputation: public SpellScriptLoader
 {
 public:
-    spell_gen_oracle_wolvar_reputation() : SpellScriptLoader("spell_gen_oracle_wolvar_reputation") { }
+   spell_gen_oracle_wolvar_reputation() : SpellScriptLoader("spell_gen_oracle_wolvar_reputation") { }
 
-    class spell_gen_oracle_wolvar_reputation_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(spell_gen_oracle_wolvar_reputation_SpellScript)
+   class spell_gen_oracle_wolvar_reputation_SpellScript : public SpellScript
+   {
+       PrepareSpellScript(spell_gen_oracle_wolvar_reputation_SpellScript)
 
-        void HandleDummy(SpellEffIndex effIndex)
-        {                 
-            
-            if (Player* player = GetCaster()->ToPlayer())
-            {
+       void HandleDummy(SpellEffIndex effIndex)
+       {                 
+           
+           if (Player* player = GetCaster()->ToPlayer())
+           {
 
-                uint32 factionId = GetSpellInfo()->Effects[effIndex].CalcValue();
-                int32  repChange =  GetSpellInfo()->Effects[EFFECT_1].CalcValue();
+               uint32 factionId = GetSpellInfo()->Effects[effIndex].CalcValue();
+               int32  repChange =  GetSpellInfo()->Effects[EFFECT_1].CalcValue();
 
-                FactionEntry const* factionEntry = sFactionStore.LookupEntry(factionId);
+               FactionEntry const* factionEntry = sFactionStore.LookupEntry(factionId);
 
-                if (!factionEntry)
-                    return;
+               if (!factionEntry)
+                   return;
 
-                // Set rep to baserep + basepoints (expecting spillover for oposite faction -> become hated)
-                // Not when player already has equal or higher rep with this faction
-                if (player->GetReputationMgr().GetBaseReputation(factionEntry) < repChange)
-                    player->GetReputationMgr().SetReputation(factionEntry, repChange);
+               // Set rep to baserep + basepoints (expecting spillover for oposite faction -> become hated)
+               // Not when player already has equal or higher rep with this faction
+               if (player->GetReputationMgr().GetBaseReputation(factionEntry) < repChange)
+                   player->GetReputationMgr().SetReputation(factionEntry, repChange);
 
-                // EFFECT_INDEX_2 most likely update at war state, we already handle this in SetReputation
-            }
-              
-        }
+               // EFFECT_INDEX_2 most likely update at war state, we already handle this in SetReputation
+           }
+             
+       }
 
-        void Register()
-        {
-        OnEffect += SpellEffectFn(spell_gen_oracle_wolvar_reputation_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
-        }
-    };
+       void Register()
+       {
+       OnEffect += SpellEffectFn(spell_gen_oracle_wolvar_reputation_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+       }
+   };
 
-    SpellScript* GetSpellScript() const
-    {
-        return new spell_gen_oracle_wolvar_reputation_SpellScript();
-    }
+   SpellScript* GetSpellScript() const
+   {
+       return new spell_gen_oracle_wolvar_reputation_SpellScript();
+   }
 };
 
 void AddSC_generic_spell_scripts()
