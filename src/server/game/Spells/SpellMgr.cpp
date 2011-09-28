4149b209bc0a945dc1cd3eb2bf29f8c5c548a0c3
Origins <KillCodeZ@gmail.com>
2011-09-27 21:05:47 -0500

    Core/Spell: Hymn of Hope fix.
    thanks to: Chaplain

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3035,6 +3035,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 // with this spell atrribute aura can be stacked several times
                 spellInfo->Attributes &= ~SPELL_ATTR0_NOT_SHAPESHIFT;
                 break;
+            case 64904: //Hymn of Hope
+               spellInfo->EffectApplyAuraName[EFFECT_1] = SPELL_AURA_MOD_INCREASE_ENERGY_PERCENT;
+               break;				
             case 30421: // Nether Portal - Perseverence
                 spellInfo->Effects[2].BasePoints += 30000;
                 break;
