d75087b02048dd3b35975c17da0fcaaf310a96ae
Bootz <Stage6Dev@EMPulseGaming.com>
2011-09-30 02:26:44 -0500

    Scripts: clean-up
    * fix line endings.
    * removed extra line usage.

--- src/server/scripts/EasternKingdoms/TheStonecore/boss_priestess_azil.cpp
+++ src/server/scripts/EasternKingdoms/TheStonecore/boss_priestess_azil.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/> 
+ * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include "ScriptPCH.h"
 #include "the_stonecore.h"
 
