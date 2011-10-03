c5cdff2689e5954ba88a1a90a17185fbc076b032
Bootz <Stage6Dev@EMPulseGaming.com>
2011-10-03 04:31:12 -0500

    REPO/SCRIPTS: clean-up
    * fixed line endings
    * removed extra line spacing
    *fix trialling whitespaces
    
    DEVNOTE:
     EasternKingdoms scripting is almost completely in place...
    still to be finished, is removal of SD2 kindship... don't understand why TC
    had let this back in... or didnt clear the rest of it out. so alot of codesytling needs to change... mostly for azeroth... EventAI(EAI) needs xferred to SmartAI(SAI) for these zones. and also a brisk combing of the areas to make sure there all tied intogether as should be... You scriptors out there, EK is ready for yer touchs. ;)

--- src/server/scripts/EasternKingdoms/ThroneOfTheTides/boss_erunak_stonespeaker.cpp
+++ src/server/scripts/EasternKingdoms/ThroneOfTheTides/boss_erunak_stonespeaker.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include "ScriptPCH.h"
 #include "throne_of_the_tides.h"
 
