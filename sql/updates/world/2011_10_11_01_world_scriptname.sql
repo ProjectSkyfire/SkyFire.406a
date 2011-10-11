0c5db6eaa3be84cd9795b3d5bfbf8878d2141a03
Bootz <Stage6Dev@EMPulseGaming.com>
2011-10-11 17:46:00 -0500

    Core/Misc: Fixed portals for quest (12790)

---
+++ sql/updates/world/2011_10_11_01_world_scriptname.sql
@@ -0,0 +1,2 @@
+UPDATE `gameobject_template` SET `ScriptName`='' WHERE `entry`=191230; -- go_tele_to_dalaran_crystal
+UPDATE `gameobject_template` SET `ScriptName`='' WHERE `entry`=191229; -- go_tele_to_violet_stand
