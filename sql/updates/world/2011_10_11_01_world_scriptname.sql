479756d1312036bb8b4902b3f21f6bc7bf8e52c9
Bootz <Stage6Dev@EMPulseGaming.com>
2011-10-11 18:51:25 -0500

    Core/Quest: Fixed last commit...
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- sql/updates/world/2011_10_11_01_world_scriptname.sql
+++ sql/updates/world/2011_10_11_01_world_scriptname.sql
@@ -1,2 +1,1 @@
-UPDATE `gameobject_template` SET `ScriptName`='' WHERE `entry`=191230; -- go_tele_to_dalaran_crystal
 UPDATE `gameobject_template` SET `ScriptName`='' WHERE `entry`=191229; -- go_tele_to_violet_stand
