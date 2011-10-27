0def4380ab224707fe7f1432858b6f37a006c3f5
Origins <KillCodeZ@gmail.com>
2011-10-26 23:27:04 -0500

    Removed trash from PVS-Studio vs ext.
    was not to be pushed..

--- src/server/game/Spells/spelleffects.cpp.cl.cmd
+++
@@ -1,2 +0,0 @@
-@call "c:\program files (x86)\microsoft visual studio 10.0\vc\vcvarsall.bat" amd64 >nul
-@call "c:\program files (x86)\microsoft visual studio 10.0\vc\bin\amd64\cl.exe" @"c:\users\aptiva\desktop\sf_offical_rebase\skyfireemu_rebase\src\server\game\spells\spelleffects.cpp.cl.cfg" 2>&1
