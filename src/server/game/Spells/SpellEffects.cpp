284cb67ebafe9b76e241c9c9639a70703c95e6b1
Subv2112 <s.v.h21@hotmail.com>
2011-10-28 17:46:14 -0500

    Moved a bunch of warrior spells to SpellScripts, fixed Blood And Thunder, still more to do

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -278,26 +278,6 @@ void Spell::EffectInstaKill(SpellEffIndex /*effIndex*/)
     if (!unitTarget || !unitTarget->isAlive())
         return;
 
-    // Demonic Sacrifice
-    if (m_spellInfo->Id == 18788 && unitTarget->GetTypeId() == TYPEID_UNIT)
-    {
-        uint32 entry = unitTarget->GetEntry();
-        uint32 spellID;
-        switch (entry)
-        {
-            case   416: spellID = 18789; break;               //imp
-            case   417: spellID = 18792; break;               //fellhunter
-            case  1860: spellID = 18790; break;               //void
-            case  1863: spellID = 18791; break;               //succubus
-            case 17252: spellID = 35701; break;               //fellguard
-            default:
-                sLog->outError("EffectInstaKill: Unhandled creature entry (%u) case.", entry);
-                return;
-        }
-
-        m_caster->CastSpell(m_caster, spellID, true);
-    }
-
     if (m_caster == unitTarget)                              // prevent interrupt message
         finish();
 
@@ -479,69 +459,6 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                         damage = (m_caster->getLevel() - 60) * 4 + 60;
                         break;
                     }
-                }
-                break;
-            }
-            case SPELLFAMILY_WARRIOR:
-            {
-               // Bloodthirst
-               if (m_spellInfo->SpellFamilyFlags[1] & 0x400)
-                   damage = uint32(damage * (m_caster->GetTotalAttackPowerValue(BASE_ATTACK)) / 100);
-               // Victory Rush
-               else if (m_spellInfo->SpellFamilyFlags[1] & 0x100)
-               {
-                   damage = uint32(damage * m_caster->GetTotalAttackPowerValue(BASE_ATTACK) / 100);
-                   m_caster->RemoveAurasDueToSpell(32216); // Victorious
-               }
-
-               // Cleave
-               else if (m_spellInfo->Id == 845)
-                   damage = uint32(6+ m_caster->GetTotalAttackPowerValue(BASE_ATTACK)* 0.45);
-               // Intercept
-               else if (m_spellInfo->Id == 20253)
-                   damage = uint32(1 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.12);
-                else if (m_spellInfo->Id ==5308) // Execute
-                {
-                    float ap = m_caster->GetTotalAttackPowerValue(BASE_ATTACK);
-                    damage = uint32 (10 + ap * 0.437 * 100 / 100);
-                    uint32 power = m_caster->GetPower(POWER_RAGE);
-                    if(power > 0)
-                    {
-                        uint32 mod = power > 20 ? 20 : power;
-                        uint32 bonus_rage = 0;
-
-                        if(m_caster->HasAura(29723))
-                            bonus_rage = 5;
-                        if(m_caster->HasAura(29725))
-                            bonus_rage = 10;
-
-                        damage += uint32 ((ap * 0.874 * 100 / 100 - 1) * mod / 100.0f);
-                        m_caster->SetPower(POWER_RAGE, (power - mod) + bonus_rage);
-                    }
-                }
-                else if (m_spellInfo->Id == 78) // Heroic Strike
-                    damage = uint32(8 + (m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 60) / 100);
-                else if (m_spellInfo->Id == 20253)
-                    damage = uint32(1 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.12);
-                else if (m_spellInfo->Id == 46968) // Shockwave
-                {
-                    int32 pct = m_caster->CalculateSpellDamage(unitTarget, m_spellInfo, 2);
-                    if (pct > 0)
-                        damage += int32(CalculatePctN(m_caster->GetTotalAttackPowerValue(BASE_ATTACK), pct));
-                    break;
-                }
-                else if (m_spellInfo->Id == 6343)
-                {
-                    uint32 trig_spell;
-                    if (m_caster->HasAura(80979))
-                        trig_spell = 87095;
-                    else if (m_caster->HasAura(80980))
-                        trig_spell = 87096;
-                    else
-                        break;
-
-                    if(urand(0,1))
-                        m_caster->CastSpell(m_caster, trig_spell, true);
                 }
                 break;
             }
