8b4c2db5599a85399e5c376bdd810872eb6fcb4a
Subv2112 <s.v.h21@hotmail.com>
2011-10-31 08:23:35 -0500

    Remove old Everlasting Afflictions link from DB
    Signed-off-by: Subv2112 <s.v.h21@hotmail.com>

--- sql/updates/World/2011_10_31_world_spell_script_names.sql
+++ sql/updates/World/2011_10_31_world_spell_script_names.sql
@@ -6,3 +6,5 @@ INSERT INTO spell_script_names (spell_id,scriptname) VALUES (689,'spell_warl_dra
 
 DELETE FROM spell_script_names WHERE spell_id = 89420 OR spell_id = -89420;
 INSERT INTO spell_script_names (spell_id,scriptname) VALUES (89420,'spell_warl_drain_life');
+
+DELETE FROM spell_script_names WHERE spell_id = 47201; -- Remove this, deprecated
