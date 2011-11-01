21a85ac9cfd6a991845bab465bba048e13b8183c
Bootz <Stage6Dev@EMPulseGaming.com>
2011-11-01 09:01:14 -0500

    revert part of 36d6ec90fb80ab3232819323547829efdc2c37de

--- sql/updates/World/2011_11_01_04_world_quest_template.sql
+++
@@ -1,3 +0,0 @@
--- Fixes Monkey Business creature kill count
-UPDATE quest_template SET reqcreatureorgoid1 = 34699 WHERE entry = 14019;
-UPDATE quest_template SET ReqSpellCast1 = 67917 WHERE entry = 14019;
