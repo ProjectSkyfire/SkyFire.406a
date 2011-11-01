eea42392612d2647f50ea73db531d9e37ac6bd35
Bootz <Stage6Dev@EMPulseGaming.com>
2011-11-01 08:09:00 -0500

    Core/Revision: adjusted Legal Copyright string for date and project(s)

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -3,7 +3,7 @@
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
  #define VER_COMPANYNAME_STR        "Project SkyFire Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2011 Project SkyFire"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2010-2011 TrinityCore & Project SkyFire"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
