6f2c7ec1fd16c49cc3dfbd280c39c8ddad499caf
Bootz <Stage6Dev@EMPulseGaming.com>
2011-11-08 00:34:43 -0600

    Core/Spells: Fix earth and fire elemental totems
    Spells/DK: Temporary fix for anti-magic zone spell

--- src/server/game/Entities/Totem/Totem.cpp
+++ src/server/game/Entities/Totem/Totem.cpp
@@ -89,7 +89,17 @@ void Totem::InitSummon()
 {
     if (m_type == TOTEM_PASSIVE)
     {
-        CastSpell(this, GetSpell(), true);
+        switch (GetSpell())
+        {
+            case 33663: // Earth Elemental Totem
+            case 32982: // Fire Elemental Totem
+            case 50461: // Anti-Magic Zone
+                CastSpell(this, GetSpell(), true);
+                break;
+            default:
+                AddAura(GetSpell(), this);
+                break;
+        }
     }
 
     // Some totems can have both instant effect and passive spell
