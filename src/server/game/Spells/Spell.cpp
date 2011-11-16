c62d6db2f1fb4d338b9975eb454ec57d0f6f5b3e
Bootz <Stage6Dev@EMPulseGaming.com>
2011-11-16 05:25:51 -0600

    [Core/Spells] Disable use Mage Invisible in bg/arena when status STATUS_WAIT_JOIN
    thanks to: aios

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -4602,6 +4602,13 @@ void Spell::HandleEffects(Unit* pUnitTarget, Item* pItemTarget, GameObject* pGOT
 
 SpellCastResult Spell::CheckCast(bool strict)
 {
+    // check Blink #(1953) and Mage Invisibility (#66) on battleground
+    // or arena when status STATUS_WAIT_JOIN
+    if (m_caster->GetTypeId() == TYPEID_PLAYER)
+        if (Battleground* bg = m_caster->ToPlayer()->GetBattleground())
+            if (bg->GetStatus() == STATUS_WAIT_JOIN && (m_spellInfo->Id == 66 || m_spellInfo->Id == 1953))
+                return SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
+
     // check death state
     if (!m_caster->isAlive() && !(m_spellInfo->Attributes & SPELL_ATTR0_PASSIVE) && !((m_spellInfo->Attributes & SPELL_ATTR0_CASTABLE_WHILE_DEAD) || (IsTriggered() && !m_triggeredByAuraSpell)))
         return SPELL_FAILED_CASTER_DEAD;
