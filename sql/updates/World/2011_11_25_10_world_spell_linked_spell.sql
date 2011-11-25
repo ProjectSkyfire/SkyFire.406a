4cde2ff9ff811182922cffb0c1620da5f8efde47
netcho <kkotlarski@abv.bg>
2011-11-26 00:17:18 +0200

    Update sql/updates/World/2011_11_25_10_world_spell_linked_spell.sql

--- sql/updates/World/2011_11_25_10_world_spell_linked_spell.sql
+++ sql/updates/World/2011_11_25_10_world_spell_linked_spell.sql
@@ -1,2 +1,2 @@
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('26373', '37553', 'Cast visual for Consecration');
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('26373', '81297', 'Cast damaging spell for Consecration');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('26373', '37553', '0', 'Cast visual for Consecration');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('26373', '81297', '0', 'Cast damaging spell for Consecration');
