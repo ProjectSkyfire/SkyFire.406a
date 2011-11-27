7aca247c872a4db00f30de098dcc5b971f498c6a
Subv2112 <s.v.h21@hotmail.com>
2011-11-27 16:23:19 -0500

    Remove wrong data, aura is SPELL_AURA_ADD_TARGET_TRIGGER
    Signed-off-by: Subv2112 <s.v.h21@hotmail.com>

--- sql/updates/World/2011_11_27_world_spell_proc_event.sql
+++
@@ -1,7 +0,0 @@
--- (81131) Scarlet Fever (Rank 1)
-DELETE FROM `spell_proc_event` WHERE `entry` IN (81131);
-INSERT INTO `spell_proc_event` VALUES (81131, 0x00, 0x0F, 0x00000000, 0x00000000, 0x00000002, 0x00011010, 0x00010000, 0, 100, 0);
-
--- (81132) Scarlet Fever (Rank 2)
-DELETE FROM `spell_proc_event` WHERE `entry` IN (81132);
-INSERT INTO `spell_proc_event` VALUES (81132, 0x00, 0x0F, 0x00000000, 0x00000000, 0x00000002, 0x00011010, 0x00010000, 0, 100, 0);
