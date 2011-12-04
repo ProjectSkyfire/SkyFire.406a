f871681f563cfc74adb6e01c9ffd051f349909c0
SkyFire <admin@projectskyfire.org>
2011-12-03 19:54:21 -0600

    New functions to work with Blizzard's packed streams - ByteBuffer::writeBit, ByteBuffer::writeBits.  ***IMPORTANT***  You will crash at the end of the loading screen.  This is ***NOT*** finished  ***THIS BRANCH IS NOT SUPPORTED*** Ported from https://github.com/ProjectSkyfire/SkyFireEMU_420/commit/cfd869f3b2679f3a75abe06d904cb450db62ea2b
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/shared/Packets/ByteBuffer.h
+++ src/server/shared/Packets/ByteBuffer.h
@@ -51,19 +51,19 @@ class ByteBuffer
         const static size_t DEFAULT_SIZE = 0x1000;
 
         // constructor
-        ByteBuffer(): _rpos(0), _wpos(0)
+        ByteBuffer(): _rpos(0), _wpos(0), _bitpos(8), _curbitval(0)
         {
             _storage.reserve(DEFAULT_SIZE);
         }
 
         // constructor
-        ByteBuffer(size_t res): _rpos(0), _wpos(0)
+        ByteBuffer(size_t res): _rpos(0), _wpos(0), _bitpos(8), _curbitval(0)
         {
             _storage.reserve(res);
         }
 
         // copy constructor
-        ByteBuffer(const ByteBuffer &buf): _rpos(buf._rpos), _wpos(buf._wpos), _storage(buf._storage) { }
+        ByteBuffer(const ByteBuffer &buf): _rpos(buf._rpos), _wpos(buf._wpos), _storage(buf._storage), _bitpos(8), _curbitval(0) { }
 
         void clear()
         {
@@ -75,6 +75,37 @@ class ByteBuffer
         {
             EndianConvert(value);
             append((uint8 *)&value, sizeof(value));
+
+            flushBits();
+        }
+
+        void flushBits()
+        {
+            if (_bitpos == 8)
+                return;
+
+            append((uint8 *)&_curbitval, sizeof(uint8));
+            _curbitval = 0;
+            _bitpos = 8;
+        }
+        bool writeBit(uint32 bit)
+        {
+            --_bitpos;
+            if (bit)
+                _curbitval |= (1 << (_bitpos));
+
+            if (_bitpos == 0)
+            { 
+                _bitpos = 8;
+                append((uint8 *)&_curbitval, sizeof(_curbitval));
+                _curbitval = 0;
+            }
+            return (bit != 0);
+        }
+        template <typename T> void writeBits(T value, size_t bits)
+        {
+            for (int32 i = bits-1; i >= 0; --i)
+                writeBit(value >> i & 1);
         }
 
         template <typename T> void put(size_t pos, T value)
@@ -489,7 +520,8 @@ class ByteBuffer
         }
 
     protected:
-        size_t _rpos, _wpos;
+        size_t _rpos, _wpos, _bitpos;
+        uint8 _curbitval;
         std::vector<uint8> _storage;
 };
 
