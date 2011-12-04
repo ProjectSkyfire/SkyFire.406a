0b2fb982ff38ec01453699ce6977d89ab240b2d5
Subv2112 <s.v.h21@hotmail.com>
2011-12-03 19:22:34 -0500

    Corrected comment, thanks DJScias

--- sql/updates/World/2011_12_03_02_world_smart_scripts.sql
+++ sql/updates/World/2011_12_03_02_world_smart_scripts.sql
@@ -9,5 +9,5 @@ SET @SOURCETYPE := 0;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,@SOURCETYPE,0,1,8,0,100,1,79751,0,100,120,33,42224,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"Repaired Mechano-Tank - On Spell Hit - Give Kill Credit and die"),
+(@ENTRY,@SOURCETYPE,0,1,8,0,100,1,79751,0,100,120,33,42224,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"Repaired Mechano-Tank - On Spell Hit - Give Kill Credit"),
 (@ENTRY,@SOURCETYPE,1,0,61,0,100,0,0,0,0,0,37,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Repaired Mechano-Tank - On Spell Hit - Die");
