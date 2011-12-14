033476d826bd0bbdc36bae4846b46651c67d6700
Bootz <Stage6Dev@EMPulseGaming.com>
2011-12-14 00:25:57 -0600

    Scripts/Maelstrome: Kezan
    * Added quest 14069

--- src/server/scripts/Maelstrom/deepholm.cpp
+++ src/server/scripts/Maelstrom/deepholm.cpp
@@ -127,7 +127,7 @@ class npc_slaincrewmember : public CreatureScript
                         break;
                 }
             }
-			else sayTimer -= diff;
+            else sayTimer -= diff;
         }
     };
 
