19be50253899e73df3dc0b4967292ca64a3b27f1
Bootz <Stage6Dev@EMPulseGaming.com>
2011-12-14 21:57:03 -0600

    Core/Vmap: Remove a Check for not existing map

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1248,7 +1248,8 @@ void World::SetInitialWorldSettings()
         || !MapManager::ExistMapAndVMap(1, -2917.58f, -257.98f)
         || (m_int_configs[CONFIG_EXPANSION] && (
             !MapManager::ExistMapAndVMap(530, 10349.6f, -6357.29f) ||
-            !MapManager::ExistMapAndVMap(530, -3961.64f, -13931.2f))))
+            !MapManager::ExistMapAndVMap(530, -3961.64f, -13931.2f) ||
+            !MapManager::ExistMapAndVMap(648, -8423.809570f, 1361.300049f))))			
     {
         sLog->outError("Correct *.map files not found in path '%smaps' or *.vmtree/*.vmtile files in '%svmaps'. Please place *.map/*.vmtree/*.vmtile files in appropriate directories or correct the DataDir value in the worldserver.conf file.", m_dataPath.c_str(), m_dataPath.c_str());
         exit(1);
