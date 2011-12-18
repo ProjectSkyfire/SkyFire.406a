aaa64c5b899b834d40be90bf5972fc1529ec6d17
horn <pankrac.ja@seznam.cz>
2011-12-18 19:33:52 +0100

    Core/Achievements: Remove stupid break which caused achievement Fa-la-la-la Ogri'la can't be completed.
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -1152,7 +1152,6 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     AchievementCriteriaDataSet const* data = sAchievementMgr->GetCriteriaDataSet(achievementCriteria);
                     if (!data || !data->Meets(GetPlayer(), unit))
                         continue;
-                    break;
                 }
 
                 SetCriteriaProgress(achievementCriteria, 1);
