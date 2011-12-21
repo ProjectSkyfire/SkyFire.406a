fbbd98a46da0ccb97d900ed5b34bb5f31b31dfd1
Bootz <Stage6Dev@EMPulseGaming.com>
2011-12-21 15:30:50 -0600

    DB: removed unneeded addition
    (good catch subv)

--- sql/updates/world/2011_12_21_03_worldspell_linked_spell.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (56641, 77443, 1, 'Steady Shot Focus');
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (77767, 91954, 1, 'Cobra Shot Focus');
