6f7828e0a0fcce1d26742929ab3a7da2637881f4
Subv2112 <s.v.h21@hotmail.com>
2011-12-27 16:36:54 -0500

    Attemt #2 of fixing mingw and msys compile, now ACE almost builds, just need to fix the "Undefined reference to Netbios@4" linker error
    Signed-off-by: Subv2112 <s.v.h21@hotmail.com>

--- dep/g3dlite/include/G3D/platform.h
+++ dep/g3dlite/include/G3D/platform.h
@@ -49,7 +49,7 @@
 #define __fastcall
 #endif
 
-#ifdef _MSC_VER 
+#if defined(_MSC_VER) || defined(_WIN32) || defined(WIN32) || defined(__MINGW32__)
     #define G3D_WIN32
 #elif  defined(__FreeBSD__) || defined(__OpenBSD__)
     #define G3D_FREEBSD
