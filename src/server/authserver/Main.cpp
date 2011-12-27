6ed4ab0858c2e172b9a8da89db76e29e9715d5d9
Subv2112 <s.v.h21@hotmail.com>
2011-12-27 11:58:24 -0500

    Use correct criteria type, removed code that was not supposed to be commited
    Signed-off-by: Subv2112 <s.v.h21@hotmail.com>

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -33,7 +33,6 @@
 #include <ace/Sig_Handler.h>
 #include <openssl/opensslv.h>
 #include <openssl/crypto.h>
-#include <vld.h>
 
 #ifndef _AUTHSERVER_CONFIG
 # define _AUTHSERVER_CONFIG  "authserver.conf"
