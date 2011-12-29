c3e3db5bea3cc23f90e3a53a5be26cb83bb11597
w1sht0l1v3 <w1sht0l1v3@gmail.com>
2011-12-28 18:01:47 -0600

    Core/Achievements: Remove hardcoded script and add proper achievement criteria data in DB.
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -1171,14 +1171,6 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                         continue;
                 }
 
-                if (achievement->ID == 1282)
-                {
-                    // those requirements couldn't be found in the dbc
-                    AchievementCriteriaDataSet const* data = sAchievementMgr->GetCriteriaDataSet(achievementCriteria);
-                    if (!data || !data->Meets(GetPlayer(), unit))
-                        continue;
-                }
-
                 SetCriteriaProgress(achievementCriteria, 1);
                 break;
             }
