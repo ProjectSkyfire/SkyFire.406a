ProjectSkyfire/SkyFire.406a

commit 2536c1e29675be9eaa1b756e69a84478f5470bd8

Core/Player: Fixed crash while login in world. Now you can go in world.

src/server/game/Entities/Player/Player.cpp
@@ -17194,8 +17194,8 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
     //"resettalents_time, trans_x, trans_y, trans_z, trans_o, transguid, extra_flags, at_login, zone, online, death_expire_time, taxi_path, instance_mode_mask, "
     // 38           39                40                 41                    42          43          44              45           46               47              48
     //"arenaPoints, totalHonorPoints, todayHonorPoints, yesterdayHonorPoints, totalKills, todayKills, yesterdayKills, chosenTitle, knownCurrencies, watchedFaction, drunk, "
-    // 49      50      51      52      53      54      55      56      57      58      69       60           61         62          63             64              65      66           67            68               69            70
-    //"health, power1, power2, power3, power4, power5, power6, power7, power8, power9, power10, instance_id, speccount, activespec, exploredZones, equipmentCache, ammoId, knownTitles, actionBars, currentpetslot, petslotused, grantableLevels FROM characters WHERE guid = '%u'", guid);
+    // 49      50      51      52      53      54      55      56      57      58      59       60           61         62          63             64              65           66          67              68           69
+    //"health, power1, power2, power3, power4, power5, power6, power7, power8, power9, power10, instance_id, speccount, activespec, exploredZones, equipmentCache, knownTitles, actionBars, currentPetSlot, petSlotUsed, grantableLevels FROM characters WHERE guid = '%u'", guid);
     PreparedQueryResult result = holder->GetPreparedResult(PLAYER_LOGIN_QUERY_LOADFROM);
 
     if (!result)
@@ -17282,13 +17282,11 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
 
     //SetUInt64Value(PLAYER_FIELD_KNOWN_CURRENCIES, fields[47].GetUInt64());
 
-    //SetUInt32Value(PLAYER_AMMO_ID, fields[66].GetUInt32());
-
     // set which actionbars the client has active - DO NOT REMOVE EVER AGAIN (can be changed though, if it does change fieldwise)
-    SetByteValue(PLAYER_FIELD_BYTES, 2, fields[64].GetUInt8());
+    SetByteValue(PLAYER_FIELD_BYTES, 2, fields[65].GetUInt8());
 
-    m_currentPetSlot = (PetSlot)fields[68].GetUInt8();
-    m_petSlotUsed = fields[69].GetUInt32();
+    m_currentPetSlot = (PetSlot)fields[67].GetUInt8();
+    m_petSlotUsed = fields[68].GetUInt64();
 
     InitDisplayIds();
 
@@ -17822,7 +17820,7 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
     }
 
     // RaF stuff.
-    m_grantableLevels = fields[70].GetUInt32();
+    m_grantableLevels = fields[69].GetUInt32();
     if (GetSession()->IsARecruiter() || (GetSession()->GetRecruiterId() != 0))
         SetFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_REFER_A_FRIEND);
 
@@ -19196,7 +19194,7 @@ void Player::SaveToDB(bool create /*=false*/)
         stmt->setString(index++, ss.str());
         stmt->setUInt8(index++, GetByteValue(PLAYER_FIELD_BYTES, 2));
         stmt->setUInt8(index++, m_currentPetSlot);
-        stmt->setUInt32(index++, m_petSlotUsed);
+        stmt->setUInt64(index++, m_petSlotUsed);
         stmt->setUInt32(index++, m_grantableLevels);
     }
     else
@@ -19307,7 +19305,7 @@ void Player::SaveToDB(bool create /*=false*/)
         stmt->setString(index++, ss.str());
         stmt->setUInt8(index++, GetByteValue(PLAYER_FIELD_BYTES, 2));
         stmt->setUInt8(index++, m_currentPetSlot);
-        stmt->setUInt32(index++, m_petSlotUsed);
+        stmt->setUInt64(index++, m_petSlotUsed);
         stmt->setUInt32(index++, m_grantableLevels);
 
         stmt->setUInt8(index++, IsInWorld() ? 1 : 0);

sql/updates/characters/2011_12_20_01_characters_character.sql
@@ -1,0 +1,4 @@
+ALTER TABLE `characters`
+  CHANGE COLUMN `petSlotUsed` `petSlotUsed` BIGINT(10) NULL DEFAULT NULL AFTER `currentPetSlot`,
+  CHANGE COLUMN `grantableLevels` `grantableLevels` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `petSlotUsed`,
+  DROP COLUMN `ammoId`;

commit 03914300ecfd805368ea38dd4e8187b263c57797

Fixed title loading broken in 2536c1e29675be9eaa1b756e69a84478f5470bd8

src/server/game/Entities/Player/Player.cpp
@@ -17314,7 +17314,7 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
     SetUInt32Value(PLAYER_XP, fields[7].GetUInt32());
 
     _LoadIntoDataField(fields[63].GetCString(), PLAYER_EXPLORED_ZONES_1, PLAYER_EXPLORED_ZONES_SIZE);
-    _LoadIntoDataField(fields[66].GetCString(), PLAYER__FIELD_KNOWN_TITLES, KNOWN_TITLES_SIZE*2);
+    _LoadIntoDataField(fields[65].GetCString(), PLAYER__FIELD_KNOWN_TITLES, KNOWN_TITLES_SIZE*2);
 
     SetFloatValue(UNIT_FIELD_BOUNDINGRADIUS, DEFAULT_WORLD_OBJECT_SIZE);
     SetFloatValue(UNIT_FIELD_COMBATREACH, 1.5f);