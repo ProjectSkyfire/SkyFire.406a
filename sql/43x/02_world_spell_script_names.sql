5c380780d76ecbc816495fbce3ed89ae5184ae06
Snoop3r <Snooper@Azet.sk>
2012-01-07 04:16:48 +0800

    Core/Scripts: Deleted Obsolete Spell_Quest Script Curing the Sick (http://www.wowhead.com/quest=6124) (http://www.wowhead.com/quest=6129)
    Signed-off-by: Snoop3r <Snooper@Azet.sk>

--- sql/43x/02_world_spell_script_names.sql
+++ sql/43x/02_world_spell_script_names.sql
@@ -5,3 +5,6 @@ DELETE FROM spell_script_names WHERE ScriptName='spell_q55_sacred_cleansing';
 DELETE FROM spell_script_names WHERE ScriptName='spell_q5206_test_fetid_skull';
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES 
 (17271, 'spell_q27389_test_fetid_skull');
+
+-- DELETE ScriptName For Quest Curing the Sick
+DELETE FROM spell_script_names WHERE ScriptName='spell_q6124_6129_apply_salve';
