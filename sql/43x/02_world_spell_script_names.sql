596ca4beb7710f14c50c20d14fc56d389277878d
Snoop3r <Snooper@Azet.sk>
2012-01-06 20:55:55 +0100

    Core/SQL: Fixed Query Typo From Last Commit.
    Signed-off-by: Snoop3r <Snooper@Azet.sk>

--- sql/43x/02_world_spell_script_names.sql
+++ sql/43x/02_world_spell_script_names.sql
@@ -2,6 +2,6 @@
 DELETE FROM spell_scipt_names WHERE ScriptName='spell_q55_sacred_cleansing';
 
 -- Update ScriptName For Quest Marauders of Darrowshire
-DELETE FROM spell_scipt_names WHERE ScriptName='spell_q5206_test_fetid_skull';
+DELETE FROM spell_script_names WHERE ScriptName='spell_q5206_test_fetid_skull';
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES 
 (17271, 'spell_q27389_test_fetid_skull');
