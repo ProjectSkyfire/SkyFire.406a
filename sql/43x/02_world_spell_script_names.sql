9cd442636c045914f377d80e80e8e91cd5315e14
Snoop3r <Snooper@Azet.sk>
2012-01-07 03:53:03 +0800

    Core/Scripts: Spell Quest Scripts Deleted Obsolete Spell_Quest Script Morbent Fel (http://www.wowhead.com/quest=55)
    Update Spell_Quest Script Marauders of Darrowshire
    (http://www.wowhead.com/quest=5206) - OBSOLETE
    (http://www.wowhead.com/quest=27389) - NEW ONE
    
    Signed-off-by: Snoop3r <Snooper@Azet.sk>

---
+++ sql/43x/02_world_spell_script_names.sql
@@ -0,0 +1,7 @@
+-- Delete ScriptName For Quest Morbent Fel
+DELETE FROM spell_scipt_names WHERE ScriptName='spell_q55_sacred_cleansing';
+
+-- Update ScriptName For Quest Marauders of Darrowshire
+DELETE FROM spell_scipt_names WHERE ScriptName='spell_q5206_test_fetid_skull';
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES 
+(17271, 'spell_q27389_test_fetid_skull');
