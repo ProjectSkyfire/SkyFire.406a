e47549495d8dd80de00cb6ff901303039581c5f1
Snoop3r <Snooper@Azet.sk>
2012-01-07 21:36:46 +0100

    Core/ObjectDefines: Found Typo in ObjectDefines
    Signed-off-by: Snoop3r <Snooper@Azet.sk>

--- src/server/game/Entities/Object/ObjectDefines.h
+++ src/server/game/Entities/Object/ObjectDefines.h
@@ -36,7 +36,7 @@ enum HighGuid
 {
     HIGHGUID_ITEM           = 0x4000,                       // blizz 4000
     HIGHGUID_CONTAINER      = 0x4000,                       // blizz 4000
-    HIGHGUID_PLAYER         = 0x0180,                       // blizz 0180
+    HIGHGUID_PLAYER         = 0x0100,                       // blizz 0100
     HIGHGUID_GAMEOBJECT     = 0xF110,                       // blizz F110
     HIGHGUID_TRANSPORT      = 0xF120,                       // blizz F120 (for GAMEOBJECT_TYPE_TRANSPORT)
     HIGHGUID_UNIT           = 0xF130,                       // blizz F130
@@ -46,7 +46,7 @@ enum HighGuid
     HIGHGUID_CORPSE         = 0xF101,                       // blizz F100
     HIGHGUID_MO_TRANSPORT   = 0x1FC0,                       // blizz 1FC0 (for GAMEOBJECT_TYPE_MO_TRANSPORT)
     HIGHGUID_GROUP          = 0x1F50,
-    HIGHGUID_GUILD          = 0x1FF6,
+    HIGHGUID_GUILD          = 0x1FF,
 };
 
 #define IS_EMPTY_GUID(Guid)          (Guid == 0)
