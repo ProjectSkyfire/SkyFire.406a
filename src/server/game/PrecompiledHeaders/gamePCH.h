ProjectSkyfire/SkyFire.406a

commit e7f5c47bfe88722a8902d5f86ee6e3f947e6f32d

Added files that we almost never modify to the precompiled header

src/server/game/PrecompiledHeaders/gamePCH.h
@@ -30,3 +30,45 @@
 #include "Opcodes.h"
 #include "SharedDefines.h"
 #include "ObjectMgr.h"
+
+
+#include "AchievementMgrBase.h"
+#include "AchievementMgr.h"
+#include "GuildAchievementMgr.h"
+#include "SmartAI.h"
+#include "SmartScript.h"
+#include "SmartScriptMgr.h"
+#include "CreatureAI.h"
+#include "CreatureAIFactory.h"
+#include "CreatureAIImpl.h"
+#include "CreatureAIRegistry.h"
+#include "CreatureAISelector.h"
+#include "ScriptedCreature.h"
+#include "ScriptedEscortAI.h"
+#include "ScriptedFollowerAI.h"
+#include "ScriptedGossip.h"
+#include "ScriptedSimpleAI.h"
+#include "CreatureEventAI.h"
+#include "CreatureEventAIMgr.h"
+
+#include "CombatAI.h"
+#include "GameObjectAI.h"
+#include "GuardAI.h"
+#include "PassiveAI.h"
+#include "PetAI.h"
+#include "ReactorAI.h"
+#include "TotemAI.h"
+#include "UnitAI.h"
+#include "AddonMgr.h"
+#include "AccountMgr.h"
+#include "AuctionHouseMgr.h"
+#include "Calendar.h"
+#include "UnitEvents.h"
+#include "HostileRefManager.h"
+#include "ThreatManager.h"
+#include "DisableMgr.h"
+
+#include "ScriptSystem.h"
+#include "ConditionMgr.h"
+#include "ScriptMgr.h"
+#include "ScriptLoader.h"

commit 91f17dd89a94d4468c1b1b4377f480d1411f2f11

Core/Compile: Fixed Broken Compile By Commit (e7f5c47)
Signed-off-by: Snoop3r <Snooper@Azet.sk>

src/server/game/PrecompiledHeaders/gamePCH.h
@@ -32,7 +32,7 @@
 #include "ObjectMgr.h"
 
 
-#include "AchievementMgrBase.h"
+//#include "AchievementMgrBase.h"
 #include "AchievementMgr.h"
 #include "GuildAchievementMgr.h"
 #include "SmartAI.h"