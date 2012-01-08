455882161567333897ad6d5ffb78823f869c1045
Bootz <Stage6Dev@EMPulseGaming.com>
2012-01-09 04:42:51 +0800

    Db: Arena(s)
    * Added data to the empty table...
    * Added season 9
    * set configs for new defaults
    
    notes:
    this season started 1 week after cataclysm's release.
    view: http://www.wowwiki.com/Arena for more infos

---
+++ sql/updates/world/2012_01_08_00_world_game_event_arena_seasons.sql
@@ -0,0 +1,7 @@
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (126,3);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (127,4);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (128,5);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (129,6);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (130,7);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (131,8);
+INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (132,9);
