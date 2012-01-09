cad21728da59d5e752d7b0fe130cc794bb575ad4
Bootz <Stage6Dev@EMPulseGaming.com>
2012-01-09 12:38:43 -0600

    DB: added forgotten delete statements.

--- sql/updates/world/2012_01_08_00_world_game_event_arena_seasons.sql
+++ sql/updates/world/2012_01_08_00_world_game_event_arena_seasons.sql
@@ -1,3 +1,5 @@
+DELETE FROM `game_event_arena_seasons` WHERE `eventEntry` in (126,127,128,129,130,131,132);
+
 INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (126,3);
 INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (127,4);
 INSERT  INTO `game_event_arena_seasons`(`eventEntry`,`season`) VALUES (128,5);
--- sql/updates/world/2012_01_08_01_world_game_event.sql
+++ sql/updates/world/2012_01_08_01_world_game_event.sql
@@ -1,1 +1,2 @@
+DELETE FROM `game_event` WHERE `EventEntry` IN (132);
 INSERT  INTO `game_event`(`EventEntry`,`start_time`,`end_time`,`occurence`,`length`,`holiday`,`description`,`world_event`) VALUES (132,'0000-00-00 00:00:00','0000-00-00 00:00:00',5184000,2592000,0,'Arena Season 9',0);
