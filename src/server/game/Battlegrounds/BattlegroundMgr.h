ced6d73ae01cb86e07f7306feb91a8333f6f18f8
Venugh <venugh@gmx.net>
2012-01-11 06:42:19 +0800

    Core: Cleanup.

--- src/server/game/Battlegrounds/BattlegroundMgr.h
+++ src/server/game/Battlegrounds/BattlegroundMgr.h
@@ -42,7 +42,7 @@ struct CreateBattlegroundData
     uint32 MaxPlayersPerTeam;
     uint32 LevelMin;
     uint32 LevelMax;
-    const char* BattlegroundName;
+    char* BattlegroundName;
     uint32 MapID;
     float Team1StartLocX;
     float Team1StartLocY;
