7db744d6d65427afe91750894fdaf33b779164f8
Venugh <venugh@gmx.net>
2012-01-16 20:09:41 +0100

    Core/Misc: Separate RecordTimeDiff in World::Update for MapMgr and BattlegroundMgr. thx Shocker.

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2018,7 +2018,9 @@ void World::Update(uint32 diff)
 
     /// <li> Handle all other objects
     ///- Update objects when the timer has passed (maps, transport, creatures, ...)
+    RecordTimeDiff(NULL);
     sMapMgr->Update(diff);
+    RecordTimeDiff("UpdateMapMgr");
 
     if (sWorld->getBoolConfig(CONFIG_AUTOBROADCAST))
     {
