e9c4c14df944e5509b583e3e067b54e51ce5e266
Bootz <Stage6Dev@EMPulseGaming.com>
2012-01-21 21:16:27 -0600

    * Core/Misc: Make VMapManager2::acquireModelInstance and releaseModelInstance thread safe.

--- src/server/collision/Management/VMapManager2.cpp
+++ src/server/collision/Management/VMapManager2.cpp
@@ -248,6 +248,9 @@ namespace VMAP
 
     WorldModel* VMapManager2::acquireModelInstance(const std::string& basepath, const std::string& filename)
     {
+        //! Critical section, thread safe access to iLoadedModelFiles
+        TRINITY_GUARD(ACE_Thread_Mutex, LoadedModelFilesLock);
+        
         ModelFileMap::iterator model = iLoadedModelFiles.find(filename);
         if (model == iLoadedModelFiles.end())
         {
@@ -268,6 +271,8 @@ namespace VMAP
 
     void VMapManager2::releaseModelInstance(const std::string &filename)
     {
+        //! Critical section, thread safe access to iLoadedModelFiles
+        TRINITY_GUARD(ACE_Thread_Mutex, LoadedModelFilesLock);    
         ModelFileMap::iterator model = iLoadedModelFiles.find(filename);
         if (model == iLoadedModelFiles.end())
         {
