505480c114b861060a3ec2834ee45a7a051489d7
Bootz <Stage6Dev@EMPulseGaming.com>
2012-01-30 07:39:10 -0600

    DB/Dev.Sql: more updates to Gilneas

--- sql/updates/Dev.Sql/gilneas/world_gilneas_updates.sql
+++ sql/updates/Dev.Sql/gilneas/world_gilneas_updates.sql
@@ -459,12 +459,12 @@ INSERT INTO `waypoint_data` VALUES (34851007, 17, -1420.83, 1413.46, 36.4777, 0,
  
  
 /* 
-* sql\updates\world\2011_07_29_waypoint_scripts.sql 
+* sql\updates\world\2012_01_30_waypoint_scripts.sql 
 */ 
 REPLACE INTO `waypoint_scripts` VALUES (3485100, 0, 18, 0, 0, 0, 0, 0, 0, 0, 3485100); 
  
 /* 
-* sql\updates\world\DEV_2011_07_29_creature.sql 
+* sql\updates\world\DEV_2012_01_30_creature.sql 
 */ 
 -- 34511 Forsaken Invader (Phase 1) no spawned
 DELETE FROM `creature` WHERE `id` = 34511 and `map` = 638;
@@ -484,3 +484,49 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_king_genn_greymane_c2' WHERE 
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = '68555';
 INSERT `spell_script_names` (`spell_id`,`ScriptName`) VALUES ('68555', 'spell_keg_placed');
+
+/* 
+* sql\updates\world\DEV_2012_01_30_updates.sql 
+*/ 
+
+-- Level Change from Gwen Armstead
+UPDATE creature_template SET minlevel = 1, maxlevel = 1 WHERE entry = 35840;
+
+-- Level Change from Huntsman Blake
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35874;
+
+-- Level Change from Baron <Huntsman Blake's Pet>
+UPDATE creature_template SET minlevel = 3, maxlevel = 3 WHERE entry = 38832;
+
+-- Level Change from Vitus Darkwalker
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35869;
+
+-- Level Change from Loren the Fence
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35871;
+
+-- Level Change from Sister Almyra
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35870;
+
+-- Level Change from Celestine of the Harvest
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35873;
+
+-- Level Change from Myriam Spellwalker
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35872;
+
+-- Level Change from Sergeant Cleese
+UPDATE creature_template SET minlevel = 5, maxlevel = 5 WHERE entry = 35839;
+
+-- Creature_template_addon deleted Record from Captain Broderick
+DELETE FROM `creature_template_addon` WHERE (`entry`='50371');
+
+-- Level Change from Worgen Runt
+UPDATE creature_template SET minlevel = 3, maxlevel = 4 WHERE entry = 35188;
+UPDATE creature_template SET minlevel = 3, maxlevel = 4 WHERE entry = 35456;
+
+-- Changed Creature_template_addon Query from Vincent Hersham
+DELETE FROM creature_template_addon WHERE entry = 35123;
+INSERT INTO `creature_template_addon` (`entry`, `bytes1`) VALUES ('35123', '8');
+
+-- Insert STATE_READY2H Emote to Tobias Mistmantle
+DELETE FROM creature_template_addon WHERE entry = 35124;
+INSERT INTO `creature_template_addon` (`entry`, `emote`) VALUES ('35124', '375');
