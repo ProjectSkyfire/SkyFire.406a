2d440dd3ac688aed82d0903d4181d8e7f8101005
Bootz <Stage6Dev@EMPulseGaming.com>
2012-01-30 19:50:58 -0600

    tabs to spaces....

--- src/server/scripts/EasternKingdoms/TheStonecore/boss_corborus.cpp
+++ src/server/scripts/EasternKingdoms/TheStonecore/boss_corborus.cpp
@@ -24,57 +24,57 @@ enum Yells
 
 enum Spells
 {
-	SPELL_CRYSTAL_BARRAGE            = 86881,     // Crystal Barrage normal
-	SPELL_CRYSTAL_BARRAGE_H          = 92648,     // Crystal Barrage heroic
-	SPELL_DAMPENING_WAVE             = 82415,     // Dampening Wave normal
-	SPELL_DAMPENING_WAVE_H           = 92650,     // Dampening Wave heroic
-	SPELL_BURROW                     = 26381,     // Burrow
-	SPELL_ROCK_BORE                  = 80028,     // Rock Bore
+    SPELL_CRYSTAL_BARRAGE            = 86881,     // Crystal Barrage normal
+    SPELL_CRYSTAL_BARRAGE_H          = 92648,     // Crystal Barrage heroic
+    SPELL_DAMPENING_WAVE             = 82415,     // Dampening Wave normal
+    SPELL_DAMPENING_WAVE_H           = 92650,     // Dampening Wave heroic
+    SPELL_BURROW                     = 26381,     // Burrow
+    SPELL_ROCK_BORE                  = 80028,     // Rock Bore
 };
 
 class npc_rock_borer : public CreatureScript
 {
 public:
-	npc_rock_borer() : CreatureScript("npc_rock_borer") { }
+    npc_rock_borer() : CreatureScript("npc_rock_borer") { }
 
-	CreatureAI* GetAI(Creature* creature) const
-	{
-		return new npc_rock_borerAI(creature);
-	}
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_rock_borerAI(creature);
+    }
 
     struct npc_rock_borerAI : public ScriptedAI
-	{
-		npc_rock_borerAI(Creature* creature) : ScriptedAI(creature)
-		{
-			instance = creature->GetInstanceScript();
-		}
+    {
+        npc_rock_borerAI(Creature* creature) : ScriptedAI(creature)
+        {
+            instance = creature->GetInstanceScript();
+        }
 
         InstanceScript* instance;
 
         uint32 _SpellBoreTimer;
 
         void Reset()
-		{
-			_SpellBoreTimer = 6000;
-		}
+        {
+            _SpellBoreTimer = 6000;
+        }
 
         void EnterCombar(Unit* ) { }
 
         void MoveInLineOfSight(Unit* who) { }
 
         void UpdateAI(const uint32 Diff)
-		{
-			if (_SpellBoreTimer <= Diff)
-			{
-				DoCast(me->getVictim(),SPELL_ROCK_BORE);
-				_SpellBoreTimer = 6000;
-			}
-			else
-				_SpellBoreTimer -= Diff;
+        {
+            if (_SpellBoreTimer <= Diff)
+            {
+                DoCast(me->getVictim(),SPELL_ROCK_BORE);
+                _SpellBoreTimer = 6000;
+            }
+            else
+                _SpellBoreTimer -= Diff;
 
             DoMeleeAttackIfReady();
-		}
-	};
+        }
+    };
 };
 
 class boss_corborus : public CreatureScript
@@ -95,51 +95,51 @@ public:
         }
 
         InstanceScript* instance;
-		SummonList Summons;
+        SummonList Summons;
 
         uint32 _SummonBorerTimer;
-		uint32 b_BorrowTimer;
-		uint32 _CrystalTimer;
-		uint32 _DampeningTimer;
-		bool b_BORROW;               //bool var type for invisible mode
+        uint32 b_BorrowTimer;
+        uint32 _CrystalTimer;
+        uint32 _DampeningTimer;
+        bool b_BORROW;               //bool var type for invisible mode
 
         void Reset()
-		{
-			instance->SetData(DATA_CORBORUS_EVENT,NOT_STARTED);
-			_SummonBorerTimer    = 33000;
-			b_BORROW             = 0;
-			_CrystalTimer        = 13600;
-			_DampeningTimer      = 25000;
-			Summons.DespawnAll();
-		}
+        {
+            instance->SetData(DATA_CORBORUS_EVENT,NOT_STARTED);
+            _SummonBorerTimer    = 33000;
+            b_BORROW             = 0;
+            _CrystalTimer        = 13600;
+            _DampeningTimer      = 25000;
+            Summons.DespawnAll();
+        }
 
-		void JustDied(Unit* )
-		{
-			if (instance)
-				instance->SetData(DATA_CORBORUS_EVENT,DONE);
-			Summons.DespawnAll();
-		}
+        void JustDied(Unit* )
+        {
+            if (instance)
+                instance->SetData(DATA_CORBORUS_EVENT,DONE);
+            Summons.DespawnAll();
+        }
 
         void EnterCombat(Unit* /*who*/)
-		{
-			instance->SetData(DATA_CORBORUS_EVENT,IN_PROGRESS);
-			DoZoneInCombat();
-		}
+        {
+            instance->SetData(DATA_CORBORUS_EVENT,IN_PROGRESS);
+            DoZoneInCombat();
+        }
 
-		void SummonRockBorer()
-		{
-			if (!IsHeroic())
-			    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
-			        for (uint8 i=0;i<2;++i)                                   //summon 2 Rock Borer Normal Mode
-				me->SummonCreature(NPC_ROCK_BORER,target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(),0.0f, TEMPSUMMON_CORPSE_DESPAWN);
+        void SummonRockBorer()
+        {
+            if (!IsHeroic())
+                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
+                    for (uint8 i=0;i<2;++i)                                   //summon 2 Rock Borer Normal Mode
+                me->SummonCreature(NPC_ROCK_BORER,target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(),0.0f, TEMPSUMMON_CORPSE_DESPAWN);
 
             if (IsHeroic())
-			    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
-			        for (uint8 i=0;i<4;++i)                                  //summon 4 Rock Borer Heroic Mode
-				me->SummonCreature(NPC_ROCK_BORER,target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(),0.0f, TEMPSUMMON_CORPSE_DESPAWN);
-		}
+                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
+                    for (uint8 i=0;i<4;++i)                                  //summon 4 Rock Borer Heroic Mode
+                me->SummonCreature(NPC_ROCK_BORER,target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(),0.0f, TEMPSUMMON_CORPSE_DESPAWN);
+        }
 
-		void JustSummoned(Creature* summoned)
+        void JustSummoned(Creature* summoned)
         {
             summoned->SetInCombatWithZone();
 
@@ -150,7 +150,7 @@ public:
             Summons.Summon(summoned);
         }
 
-		void SummonedCreatureDespawn(Creature* summon)
+        void SummonedCreatureDespawn(Creature* summon)
         {
             Summons.Despawn(summon);
         }
@@ -158,54 +158,54 @@ public:
         void UpdateAI(const uint32 Diff)
         {
             if (!UpdateVictim())
-				return;
+                return;
 
-			if (_SummonBorerTimer <= Diff)
-			{
-				instance->DoSendNotifyToInstance("INSTANCE MESSAGE: Rock Borer are spawned"); // Notify to players when Rock Borer are spawn
-				b_BORROW = 1;
-				DoCast(me,SPELL_BURROW);
-				SummonRockBorer();
-				_SummonBorerTimer = 30000;
-				b_BorrowTimer = 9000;
-			}
-			else
-			{
-				_SummonBorerTimer -= Diff;
-				b_BorrowTimer -= Diff;
-			}
+            if (_SummonBorerTimer <= Diff)
+            {
+                instance->DoSendNotifyToInstance("INSTANCE MESSAGE: Rock Borer are spawned"); // Notify to players when Rock Borer are spawn
+                b_BORROW = 1;
+                DoCast(me,SPELL_BURROW);
+                SummonRockBorer();
+                _SummonBorerTimer = 30000;
+                b_BorrowTimer = 9000;
+            }
+            else
+            {
+                _SummonBorerTimer -= Diff;
+                b_BorrowTimer -= Diff;
+            }
             if (b_BorrowTimer <= Diff)
-				b_BORROW = 0;
+                b_BORROW = 0;
 
-			if (_CrystalTimer <= Diff)
-			{
-				if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true))
-					if (!IsHeroic())
-					{
+            if (_CrystalTimer <= Diff)
+            {
+                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true))
+                    if (!IsHeroic())
+                    {
                         DoCast(target,SPELL_CRYSTAL_BARRAGE);
-					}
-					else
+                    }
+                    else
                         DoCast(target,SPELL_CRYSTAL_BARRAGE_H);
 
                     _CrystalTimer = 11000;
-			}
-			else
-				_CrystalTimer -= Diff;
+            }
+            else
+                _CrystalTimer -= Diff;
 
             if (_DampeningTimer <= Diff)
-			{
-				if (!IsHeroic())
-					DoCast(SPELL_DAMPENING_WAVE);
-				else
-					DoCast(SPELL_DAMPENING_WAVE_H);
+            {
+                if (!IsHeroic())
+                    DoCast(SPELL_DAMPENING_WAVE);
+                else
+                    DoCast(SPELL_DAMPENING_WAVE_H);
 
                 _DampeningTimer = 20000;
-			}
-			else
-				_DampeningTimer -= Diff;
+            }
+            else
+                _DampeningTimer -= Diff;
 
-			if (b_BORROW == 0)                    //if not invisible
-				DoMeleeAttackIfReady();
+            if (b_BORROW == 0)                    //if not invisible
+                DoMeleeAttackIfReady();
         }
     };
 };
