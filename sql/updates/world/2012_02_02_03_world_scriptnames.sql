de3107efb8941ad0ce57499d0a4021b2eb4e44a1
burnham <georgiosburnham@gmail.com>
2012-02-02 22:03:39 +0800

    Core/Spell:  Removed some scripta removed in Patch 4.0.1 * Warlock, Spell Ritual of Doom "18541" - Removed with Patch 4.0.1 http://www.wowwiki.com/Ritual_of_Doom * Death Knight, Spell Deflection "49145" - Removed with Patch 4.0.1 http://www.wowwiki.com/Spell_Deflection * Hunter, Spell Aspect of the Beast "13161" - Removed with Patch 4.0.1 http://www.wowwiki.com/Aspect_of_the_Beast * Druid, Spell Primal Tenacity "33851" - Removed with Patch 4.0.1 http://www.wowwiki.com/Primal_Tenacity <-- not have hardcode script
    Thanks Dante for your help.
    
    Signed-off-by: burnham <georgiosburnham@gmail.com>

---
+++ sql/updates/world/2012_02_02_03_world_scriptnames.sql
@@ -0,0 +1,12 @@
+-- Warlock, Spell Ritual of Doom "18541" - Removed with Patch 4.0.1 http://www.wowwiki.com/Ritual_of_Doom
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_warl_ritual_of_doom_effect';
+
+-- Death Knight, Spell Deflection "49145" - Removed with Patch 4.0.1 http://www.wowwiki.com/Spell_Deflection
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_spell_deflection';
+
+-- Hunter, Spell Aspect of the Beast "13161" - Removed with Patch 4.0.1 http://www.wowwiki.com/Aspect_of_the_Beast
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_hun_aspect_of_the_beast';
+
+-- Druid, Spell Primal Tenacity "33851" - Removed with Patch 4.0.1 http://www.wowwiki.com/Primal_Tenacity
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dru_primal_tenacity'; -- not have hardcode script
+
