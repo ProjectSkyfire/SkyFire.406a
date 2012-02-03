44a812e94c015901b363a0943b4253a09a623a00
burnham <georgiosburnham@gmail.com>
2012-02-03 01:51:51 +0100

    Core/Spell: Paladin, Spell Judgement of Command "20425" - Removed with Patch 4.0.1 http://www.wowwiki.com/Judgement_of_Command
    Signed-off-by: burnham <georgiosburnham@gmail.com>

---
+++ sql/updates/world/2012_02_03_01_world_spell_scriptnames.sql
@@ -0,0 +1,3 @@
+-- Paladin, Spell Judgement of Command "20425" - Removed with Patch 4.0.1 http://www.wowwiki.com/Judgement_of_Command
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pal_judgement_of_command';
+
