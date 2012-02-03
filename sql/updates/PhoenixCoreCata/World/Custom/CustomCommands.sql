421b5628ef60001966c43219499fc7fee51847de
burnham <georgiosburnham@gmail.com>
2012-02-04 00:09:29 +0100

    Custom: Added modadd command to allow moderators to give player specific items or spells by classes. [patch 008] - NOTE: "moderator_add" table need to be fill. by Fredi Machado
    Signed-off-by: burnham <georgiosburnham@gmail.com>

--- sql/updates/PhoenixCoreCata/World/Custom/CustomCommands.sql
+++ sql/updates/PhoenixCoreCata/World/Custom/CustomCommands.sql
@@ -20,3 +20,22 @@ INSERT INTO `command` VALUES
 ('baninfo playeraccount', 3, 'Syntax: .baninfo playeraccount $playerName\r\nWatch full information about a specific ban.'),
 ('banlist playeraccount', 3, 'Syntax: .banlist playeraccount [$Name]\r\nSearches the banlist for accounts according to a character name pattern.');
 
+-- Moderator Add Command
+DROP TABLE IF EXISTS `moderator_add`;
+CREATE TABLE `moderator_add` (
+  `type` tinyint(1) unsigned NOT NULL DEFAULT '0', -- 0 = item, 1 = spell
+  `id` int(10) unsigned NOT NULL DEFAULT '0', -- item or spell id,
+  `class` int(10) unsigned NOT NULL DEFAULT '0', -- restricted character class,
+  `description` varchar(30) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
+
+-- CLASS_WARRIOR       = 1,
+-- CLASS_PALADIN       = 2,
+-- CLASS_HUNTER        = 3,
+-- CLASS_ROGUE         = 4,
+-- CLASS_PRIEST        = 5,
+-- CLASS_DEATH_KNIGHT  = 6,
+-- CLASS_SHAMAN        = 7,
+-- CLASS_MAGE          = 8,
+-- CLASS_WARLOCK       = 9,
+-- CLASS_DRUID         = 11
