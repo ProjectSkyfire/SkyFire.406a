a172040f4e22f8ea8ddf8cbe418a1a628339461c
burnham <georgiosburnham@gmail.com>
2012-02-03 20:12:28 +0100

    Custom: Automatic annouce when player is banned [patch 005]
    Signed-off-by: burnham <georgiosburnham@gmail.com>

--- sql/updates/PhoenixCoreCata/World/Custom/CustomCommands.sql
+++ sql/updates/PhoenixCoreCata/World/Custom/CustomCommands.sql
@@ -5,3 +5,12 @@
 DELETE FROM `skyfire_string` WHERE `entry`=821;
 INSERT INTO `skyfire_string` (`entry`,`content_default`,`content_loc6`,`content_loc7`) VALUES 
 (821,'%s |cff04ac00Received Item %u. |cffd50303WARNING! If you try to add items without reason you will be fired! All items added by you are saved in the server log file.','%s |cff04ac00Received Item %u. |cffd50303WARNING! Si tratas de dar un ITEM sin razón valida y sin comunicarlo serás EXPULSADO DEL STAFF! Todos los ITEMS dados por ti son grabados en un historial.','%s |cff04ac00Received Item %u. |cffd50303WARNING! Si tratas de dar un ITEM sin razón valida y sin comunicarlo serás EXPULSADO DEL STAFF! Todos los ITEMS dados por ti son grabados en un historial.');
+
+-- Automatic annouce when player is banned
+DELETE FROM `skyfire_string` WHERE `entry`='11002';
+INSERT INTO `skyfire_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES 
+('11002','|cffff0000[GM]:|r %s was banned by %s for %s. Reason: %s.',NULL,NULL,NULL,NULL,NULL,'|cffff0000[GM]:|r %s ha sido baneado por %s por %s. La Razón es: %s.','|cffff0000[GM]:|r %s ha sido baneado por %s por %s. La Razón es: %s.',NULL);
+DELETE FROM `skyfire_string` WHERE `entry`='11003';
+INSERT INTO `skyfire_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES 
+('11003','|cffff0000[GM]:|r %s was banned by %s for eternity. Reason: %s.',NULL,NULL,NULL,NULL,NULL,'|cffff0000[GM]:|r %s ha sido baneado por %s eternamente. La Razón es: %s.','|cffff0000[GM]:|r %s ha sido baneado por %s eternamente. La Razón es: %s.',NULL);
+
--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1065,6 +1065,8 @@ enum SkyFireStrings
     // Use for custom patches             11000-11999
     LANG_AUTO_BROADCAST                 = 11000,
     LANG_INVALID_REALMID                = 11001,
+    LANG_BAN_WORLD_ANNOUNCE             = 11002,
+    LANG_PERMBAN_WORLD_ANNOUNCE         = 11003,
 
     // NOT RESERVED IDS                   12000-1999999999
     // `db_script_string` table index     2000000000-2000009999 (MIN_DB_SCRIPT_STRING_ID-MAX_DB_SCRIPT_STRING_ID)
