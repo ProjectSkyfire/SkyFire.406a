14248722380091284423c22790fa1f658efdba8c
Silviu <silviu086@gmail.com>
2012-02-03 10:11:26 +0200

    Update sql/updates/world/2012_02_01_00_world_creature_text.sql

--- sql/updates/world/2012_02_01_00_world_creature_text.sql
+++ sql/updates/world/2012_02_01_00_world_creature_text.sql
@@ -1,10 +1,10 @@
 DELETE FROM `creature_text` WHERE `entry` IN (36612);
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
-('36612','0','0','This is the beginning AND the end, mortals. None may enter the master\'s sanctum!','1','0','100','0','0','0','SAY_ENTER_ZONE'),
-('36612','1','0','The Scourge will wash over this world as a swarm of death and destruction!','1','0','100','0','0','0','SAY_AGGRO'),
-('36612','2','0','BONE STORM!','1','0','100','0','0','0','SAY_BONE_STORM'),
-('36612','3','0','Bound by bone!','1','0','100','0','0','0','SAY_BONESPIKE'),
-('36612','4','0','More bones for the offering!','1','0','100','0','0','0','SAY_KILL'),
-('36612','5','0','I see... only darkness...','1','0','100','0','0','0','SAY_DEATH'),
-('36612','6','0','THE MASTER\'S RAGE COURSES THROUGH ME!','1','0','100','0','0','0','SAY_BERSERK'),
-('36612','7','0','You Sunreaver Lackeys are all the same.','1','0','100','0','0','0','EMOTE_BONE_STORM');
+('36612','0','0','This is the beginning AND the end, mortals. None may enter the master\'s sanctum!','12','0','100','0','0','0','SAY_ENTER_ZONE'),
+('36612','1','0','The Scourge will wash over this world as a swarm of death and destruction!','12','0','100','0','0','0','SAY_AGGRO'),
+('36612','2','0','BONE STORM!','12','0','100','0','0','0','SAY_BONE_STORM'),
+('36612','3','0','Bound by bone!','12','0','100','0','0','0','SAY_BONESPIKE'),
+('36612','4','0','More bones for the offering!','12','0','100','0','0','0','SAY_KILL'),
+('36612','5','0','I see... only darkness...','12','0','100','0','0','0','SAY_DEATH'),
+('36612','6','0','THE MASTER\'S RAGE COURSES THROUGH ME!','12','0','100','0','0','0','SAY_BERSERK'),
+('36612','7','0','You Sunreaver Lackeys are all the same.','12','0','100','0','0','0','EMOTE_BONE_STORM');
