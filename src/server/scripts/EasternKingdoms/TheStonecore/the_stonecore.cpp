023f9a0c866a3c1e9cc61cc5593178181d44f45f
Bootz <bootz@projectskyfire.org>
2012-02-07 07:05:54 -0600

    Fixed UNIT_STATE*

--- src/server/scripts/EasternKingdoms/TheStonecore/the_stonecore.cpp
+++ src/server/scripts/EasternKingdoms/TheStonecore/the_stonecore.cpp
@@ -147,7 +147,7 @@ public:
 
             events.Update(diff);
 
-            if (me->HasUnitState(UNIT_STAT_CASTING))
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
 
             while (uint32 eventId = events.ExecuteEvent())
@@ -200,7 +200,7 @@ public:
 
             events.Update(diff);
 
-            if (me->HasUnitState(UNIT_STAT_CASTING))
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
 
             while (uint32 eventId = events.ExecuteEvent())
@@ -309,7 +309,7 @@ public:
 
             events.Update(diff);
 
-            if (me->HasUnitState(UNIT_STAT_CASTING))
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
 
             while (uint32 eventId = events.ExecuteEvent())
