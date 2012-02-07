0ebca925baee07b14cacad82637aa8c6246d066b
Bootz <bootz@projectskyfire.org>
2012-02-07 15:54:22 -0600

    Sripts/EaternKingdoms: Baradin Hold
    * Removed Boss Occuthar"s placeholders.
    * Occul'thar is the second boss in Baradin Hold (added in Patch 4.2.0). Similar to the Vault of Archavon, the winning faction of Tol Barad has access to this boss.
    
    Signed-off-by: burnham <georgiosburnham@gmail.com>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -90,7 +90,6 @@ void AddSC_boss_galvangar();
 void AddSC_boss_vanndar();
 void AddSC_instance_baradin_hold();          //Baradin Hold
 void AddSC_boss_argaloth();
-void AddSC_boss_occuthar();
 void AddSC_instance_bastion_of_twilight();   //BastionOfTwilight
 void AddSC_boss_theralion_and_valiona();
 void AddSC_boss_sinestra();
@@ -778,7 +777,6 @@ void AddEasternKingdomsScripts()
     AddSC_boss_vanndar();
     AddSC_instance_baradin_hold();          //Baradin Hold
     AddSC_boss_argaloth();
-    AddSC_boss_occuthar();
     AddSC_instance_bastion_of_twilight();   //BastionOfTwilight
     AddSC_boss_theralion_and_valiona();
     AddSC_boss_sinestra();
