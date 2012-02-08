5595b570371452a7607d0b747d8b34b208d945b4
Bootz <bootz@projectskyfire.org>
2012-02-08 10:34:53 -0600

    db: one last correction. commit 6be848f92e7a4a58fe8c26e0818bd0c4e47f7586
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- sql/updates/world/2012_02_08_09_world_game_event.sql
+++ sql/updates/world/2012_02_08_09_world_game_event.sql
@@ -1,1 +1,2 @@
-UPDATE `game_event` SET `start_time`= '2012-02-05 00:01:00', `holiday`=423 WHERE `eventEntry`=8; -- Love is in the Air
+UPDATE `game_event` SET `start_time`= '2012-02-05 00:01:00', `holiday`=423 WHERE `eventEntry`=8; -- Love is in the Air (Aokromes)
+UPDATE `game_event` SET `lenght`=20160 WHERE `eventEntry`=8; -- Love is in the Air (furion)
