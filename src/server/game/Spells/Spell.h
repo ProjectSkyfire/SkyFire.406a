a5f93ea9394994482689aeec364efe12e4d02251
Venugh <venugh@gmx.net>
2012-02-28 21:45:48 +0100

    Core/Spells: Changes in code related to spell explicit target redirection (cleanups, added target validation, prevent unexpected behaviour in some cases)
    by QAston.

--- src/server/game/Spells/Spell.h
+++ src/server/game/Spells/Spell.h
@@ -420,6 +420,7 @@ class Spell
         void WriteSpellGoTargets(WorldPacket* data);
 
         void InitExplicitTargets(SpellCastTargets const& targets);
+        void SelectExplicitTargets();
         void SelectSpellTargets();
         void SelectEffectTypeImplicitTargets(uint8 effIndex);
         uint32 SelectEffectTargets(uint32 i, SpellImplicitTargetInfo const& cur);
