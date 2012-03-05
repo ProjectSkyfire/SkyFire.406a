c217bc87f675d3b363f6e52870fc675647ba8204
SkyFire <admin@projectskyfire.org>
2012-03-05 15:11:51 -0600

    Fix a stupid warning.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1695,8 +1695,8 @@ enum TrapSpells
     NPC_VIPER                    = 19921
 };
 
-#define VENOMOUS_SNAKE_TIMER   1500;
-#define VIPER_TIMER            3000;
+#define VENOMOUS_SNAKE_TIMER   1500
+#define VIPER_TIMER            3000
 
 class npc_snake_trap : public CreatureScript
 {
