b40b560ed79a7d5c62b0e34310c30867e1b6aa76
Remake <havoc.remake@gmail.com>
2012-03-07 00:25:00 +0100

    Core/Unit: Define eclipse variable as private.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -17902,9 +17902,9 @@ void SpellModifier::Recalculate(SpellInfo const *spellInfo, Unit* target)
 
 void Unit::SetEclipsePower(int32 power)
 {
-    eclipse = power;
+    _eclipse = power;
 
-    if (eclipse == 0)
+    if (_eclipse == 0)
     {
         if (HasAura(67483))
             RemoveAurasDueToSpell(67483);
@@ -17912,19 +17912,19 @@ void Unit::SetEclipsePower(int32 power)
             RemoveAurasDueToSpell(67484);
     }
 
-    if (eclipse >= 100)
+    if (_eclipse >= 100)
     {
         if (HasAura(48518))
             RemoveAurasDueToSpell(48518);
-        eclipse = 100;
+        _eclipse = 100;
         AddAura(48517, ToPlayer());
     }
 
-    if (eclipse <= -100)
+    if (_eclipse <= -100)
     {
         if (HasAura(48517))
             RemoveAurasDueToSpell(48517);
-        eclipse = -100;
+        _eclipse = -100;
         AddAura(48518, ToPlayer());
     }
 
@@ -17932,7 +17932,7 @@ void Unit::SetEclipsePower(int32 power)
     data.append(GetPackGUID());
     data << int32(1);
     data << int8(POWER_ECLIPSE);
-    data << int32(eclipse);
+    data << int32(_eclipse);
     SendMessageToSet(&data, GetTypeId() == TYPEID_PLAYER ? true : false);
 }
 
--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2259,8 +2259,7 @@ class Unit : public WorldObject
         Spell* m_spellModTakingSpell;  // Spell for which charges are dropped in spell::finish
         SpellModList m_spellMods[MAX_SPELLMOD];
 
-        int32 eclipse;
-        int32 GetEclipsePower() {return eclipse;};
+        int32 GetEclipsePower() {return _eclipse;};
         void SetEclipsePower(int32 power);
 
         uint32 m_heal_done[120];
@@ -2277,12 +2276,13 @@ class Unit : public WorldObject
         void SetHealAbsorb(float absorb) { _healAbsorb = absorb; };
 
         // Movement info
-        Movement::MoveSpline * movespline;
+        Movement::MoveSpline* movespline;
 
     protected:
         explicit Unit (bool isWorldObject);
 
-        UnitAI *i_AI, *i_disabledAI;
+        UnitAI* i_AI;
+        UnitAI* i_disabledAI;
 
         void _UpdateSpells(uint32 time);
         void _DeleteRemovedAuras();
@@ -2343,8 +2343,8 @@ class Unit : public WorldObject
 
         ThreatManager m_ThreatManager;
 
-        Vehicle *m_vehicle;
-        Vehicle *_vehicleKit;
+        Vehicle* m_vehicle;
+        Vehicle* _vehicleKit;
 
         uint32 m_unitTypeMask;
 
@@ -2352,6 +2352,7 @@ class Unit : public WorldObject
         bool IsAlwaysDetectableFor(WorldObject const* seer) const;
 
         void DisableSpline();
+
     private:
         bool IsTriggeredAtSpellProcEvent(Unit* victim, Aura * aura, SpellInfo const* procSpell, uint32 procFlag, uint32 procExtra, WeaponAttackType attType, bool isVictim, bool active, SpellProcEventEntry const *& spellProcEvent);
         bool HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggeredByAura, SpellInfo const *procSpell, uint32 procFlag, uint32 procEx, uint32 cooldown);
@@ -2400,6 +2401,8 @@ class Unit : public WorldObject
 
         Spell const* _focusSpell;
         bool _targetLocked; // locks the target during spell cast for proper facing
+
+        int32 _eclipse;
 
         float _healAbsorb;
 };
@@ -2489,8 +2492,10 @@ template <class T> T Unit::ApplySpellMod(uint32 spellId, SpellModOp op, T &basev
 
         DropModCharge(mod, spell);
     }
+
     float diff = (float)basevalue * (totalmul - 1.0f) + (float)totalflat;
     basevalue = T((float)basevalue + diff);
+
     return T(diff);
 }
 #endif
