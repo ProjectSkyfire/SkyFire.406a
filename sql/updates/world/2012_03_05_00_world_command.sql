058d69c930300360c8607bf46753f1f867fe3b38
Venugh <venugh@gmx.net>
2012-03-15 15:35:07 +0100

    DB/SQL: Drop already added sqls.

--- sql/updates/world/2012_03_05_00_world_command.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM command WHERE name='note';
-INSERT INTO `command` (`name`,`security`,`help`) VALUES
-('note', 1, 'Syntax: .note  .note to make a note in the GM logs');
--- sql/updates/world/2012_03_05_01_world_skyfire_string.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM skyfire_string WHERE entry=176;
-INSERT INTO `skyfire_string` VALUES ('176', 'Note saved to logs.', null, null, null, null, null, null, null, null);
