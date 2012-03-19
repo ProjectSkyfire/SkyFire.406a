661d9585235f1c3f57d0131cecaaf36486bc1a6c
poodycliche@hotmail.com <poodycliche@hotmail.com>
2012-03-19 18:52:32 -0400

    SQL/LinkedSpell: Fix hunter talent spell Fervor,'

---
+++ sql/updates/world/2012_03_19_01_spell_linked_spell.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=82726;
+INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUE (82726,82728,0,'BM Hunter: Fervor');
