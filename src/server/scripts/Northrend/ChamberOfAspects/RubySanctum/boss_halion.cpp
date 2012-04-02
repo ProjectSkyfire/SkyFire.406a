82027ee6f9c040eee047ca791e51bc91f02e0e5b
Bootz <bootz@projectskyfire.org>
2012-04-02 17:23:52 -0500

    Scripts/Northrend: Ruby Sanctum: * removed some development sql comments. these are now in place where they should be.
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
+++ src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
@@ -1687,11 +1687,6 @@ public:
     }
 };
 
-/*
- * spell_halion_fiery_combustion 74562
- * DELETE FROM `spell_script_names` WHERE `spell_id`=74562 AND `ScriptName`='spell_halion_fiery_combustion';
- * INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (74562,'spell_halion_fiery_combustion');
- */
 class spell_halion_fiery_combustion : public SpellScriptLoader
 {
 public:
@@ -1746,11 +1741,6 @@ public:
     }
 };
 
-/*
- * spell_halion_soul_consumption 74792
- * DELETE FROM `spell_script_names` WHERE `spell_id`=74792 AND `ScriptName`='spell_halion_soul_consumption';
- * INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (74792,'spell_halion_soul_consumption');
- */
 class spell_halion_soul_consumption : public SpellScriptLoader
 {
 public:
