c8133e92f5b073c89434d5386f386209f5c05a23
Bootz <bootz@projectskyfire.org>
2012-04-10 05:34:35 -0500

    cleanup
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2415,7 +2415,7 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
                 case 62532: // Conservator's Grip
                     return false;
                 case 61716: // Rabbit Costume
-                case 61734: // Noblegarden Bunny                    
+                case 61734: // Noblegarden Bunny
                 case 30877: // Tag Murloc
                 case 62344: // Fists of Stone
                     return true;
