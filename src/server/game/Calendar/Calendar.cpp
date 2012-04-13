f13c1cbc222d9ba046b0390d3bac45bb4826d60b
Bootz <bootz@projectskyfire.org>
2012-04-12 19:42:58 -0500

    Core/Calendar: Initial work for calendar
    Original author: Silinoron
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/game/Calendar/Calendar.cpp
+++ src/server/game/Calendar/Calendar.cpp
@@ -1,11 +1,10 @@
 /*
- * Copyright (C) 2011-2012 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
+ * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -16,3 +15,5 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include "Calendar.h"
--- src/server/game/Calendar/Calendar.h
+++ src/server/game/Calendar/Calendar.h
@@ -1,11 +1,10 @@
 /*
- * Copyright (C) 2011-2012 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
+ * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -17,10 +16,64 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef SKYFIRE_CALENDAR_H
-#define SKYFIRE_CALENDAR_H
+#ifndef TRINITY_CALENDAR_H
+#define TRINITY_CALENDAR_H
 
-class Calendar
+#include "Common.h"
+
+// TODO - Get correct values
+enum CalendarEventType
 {
+    CALENDARTYPE_RAID = 0,
+    CALENDARTYPE_DUNGEON,
+    CALENDARTYPE_PVP,
+    CALENDARTYPE_MEETING,
+    CALENDARTYPE_OTHER,
 };
+
+// TODO - Get correct values
+enum CalendarInviteStatus
+{
+    CALENDARSTATUS_INVITED = 0,
+    CALENDARSTATUS_ACCEPTED,
+    CALENDARSTATUS_DECLINED,
+    CALENDARSTATUS_TENTATIVE,
+    CALENDARSTATUS_OUT,
+    CALENDARSTATUS_STANDBY,
+    CALENDARSTATUS_CONFIRMED,
+};
+
+struct CalendarEvent
+{
+    uint64 Id;
+    uint64 CreatorGuid;
+    std::string Name;
+    std::string Description;
+    uint8 Type;
+    uint8 Unk;
+    uint32 DungeonId;
+    uint32 UnkTime;
+    uint32 Time;
+    uint32 Flags;
+    uint32 GuildId;
+};
+
+struct CalendarInvite
+{
+    uint64 Id;
+    uint64 Event;
+    uint8 Status;
+    uint8 Rank;
+    uint8 Unk1;
+    uint8 Unk2;
+    uint8 Unk3;
+    std::string Text;
+    uint64 CreatorGuid;
+    uint32 Time;
+    uint64 TargetGuid;
+};
+
+typedef UNORDERED_MAP<uint64, CalendarInvite> CalendarInviteMap;
+typedef UNORDERED_MAP<uint64, CalendarEvent> CalendarEventMap;
+
 #endif
