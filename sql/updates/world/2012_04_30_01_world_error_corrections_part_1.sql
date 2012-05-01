e38c603f2023e50303290617eb476682c9003721
SkyFire <admin@projectskyfire.org>
2012-04-30 19:25:03 -0500

    Corrected some errors in item template. We are now using the DBC files to check our data in the database. More corrections to come.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

---
+++ sql/updates/world/2012_04_30_01_world_error_corrections_part_1.sql
@@ -0,0 +1,57 @@
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=20558;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=20559;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=20560;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=29024;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=29434;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=37836;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=40752;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=40753;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=41596;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=42425;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=43016;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=43228;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=43307;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=43308;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=43589;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=44990;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=45624;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=47241;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=47395;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=49426;
+UPDATE `item_template` SET `BagFamily`=0 WHERE `entry`=62898;
+
+
+UPDATE `item_template` SET `class`=12 WHERE `entry`=5165;
+UPDATE `item_template` SET `spellid_1`=0 WHERE `entry`=5522;
+UPDATE `item_template` SET `spellid_1`=0 WHERE `entry`=16321;
+UPDATE `item_template` SET `spellid_1`=0 WHERE `entry`=16322;
+UPDATE `item_template` SET `spellid_1`=0 WHERE `entry`=16323;
+UPDATE `item_template` SET `spellid_1`=0 WHERE `entry`=16325;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67132;
+UPDATE `item_template` SET `displayid`=88109 WHERE `entry`=67132;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67133;
+UPDATE `item_template` SET `displayid`=94252 WHERE `entry`=67133;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67134;
+UPDATE `item_template` SET `Material`=5 WHERE `entry`=67136;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67140;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67142;
+UPDATE `item_template` SET `displayid`=84197 WHERE `entry`=67142;
+UPDATE `item_template` SET `Material`=7 WHERE `entry`=67147;
+UPDATE `item_template` SET `displayid`=91275 WHERE `entry`=67147;
+UPDATE `item_template` SET `Material`=5 WHERE `entry`=67148;
+UPDATE `item_template` SET `displayid`=88637 WHERE `entry`=67148;
+UPDATE `item_template` SET `Material`=5 WHERE `entry`=67150;
+UPDATE `item_template` SET `displayid`=88040 WHERE `entry`=67150;
+UPDATE `item_template` SET `Material`=2 WHERE `entry`=42921;
+UPDATE `item_template` SET `Material`=4 WHERE `entry`=68660;
+UPDATE `item_template` SET `displayid`=61011 WHERE `entry`=68660;
+UPDATE `item_template` SET `class`=12 WHERE `entry`=68768;
+UPDATE `item_template` SET `Material`=4 WHERE `entry`=68768;
+UPDATE `item_template` SET `displayid`=959781 WHERE `entry`=68768;
+UPDATE `item_template` SET `class`=12 WHERE `entry`=68770;
+UPDATE `item_template` SET `Material`=4 WHERE `entry`=68770;
+UPDATE `item_template` SET `displayid`=95979 WHERE `entry`=68770;
+UPDATE `item_template` SET `class`=7 WHERE `entry`=68689;
+UPDATE `item_template` SET `Unk0`=0 WHERE `entry`=68689;
+
+
