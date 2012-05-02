dcd34f8dd3b587852dd920a478125a80ab0334bd
Bootz <bootz@projectskyfire.org>
2012-05-02 18:37:29 -0500

    DB: Cleaned up Blob Text default warning
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- sql/updates/world/2012_05_02_02_world_spell_override.sql
+++ sql/updates/world/2012_05_02_02_world_spell_override.sql
@@ -3,16 +3,16 @@ CREATE TABLE IF NOT EXISTS `spell_override` (
 	`overrideSpell` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`affSpell` INT(10) NOT NULL DEFAULT '0', 
 	`aura` INT(10) NOT NULL DEFAULT '0', 
-	`comment` LONGTEXT NOT NULL DEFAULT '',
+	`comment` LONGTEXT NOT NULL,
 	PRIMARY KEY	(`overrideSpell`)
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
 INSERT INTO spell_override VALUES
 (93402,8921,48517,"Eclipse (Solar)"),
 (84714,82731,84717,"Frostfire orb"),
-(88625,2050,0,"Chakra: Holy Word: Chastise"),   -- (aura 14751)
-(88684,88625,0,"Chakra: Holy Word: Serenity"),  -- (aura 14751)
-(88685,88625,0,"Chakra: Holy Word: Sanctuary"), -- (aura 14751)
+(88625,2050,14751,"Chakra: Holy Word: Chastise"),   -- (aura 14751)
+(88684,88625,14751,"Chakra: Holy Word: Serenity"),  -- (aura 14751)
+(88685,88625,14751,"Chakra: Holy Word: Sanctuary"),
 (86213,86121,0,"Soul Swap: Exhale"),
 (91711,6229,0,"Nether Ward Talent"),
 (92315,11366,0,"Pyroblast!"),
