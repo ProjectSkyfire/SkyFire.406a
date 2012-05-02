b036219092630d78017e7e48db759b59bc3dafde
Bootz <bootz@projectskyfire.org>
2012-05-02 12:01:33 -0500

    Core/talents: correct cost of  Dual talent specialization
    - cost has been reduced since 4.x.x, from the original 1.000g to the new amount of 10g
    thanks to: Defrag & Nevan
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>

---
+++ sql/updates/world/2012_05_02_01_gossip_menu_option.sql
@@ -0,0 +1,1 @@
+UPDATE gossip_menu_option SET box_money=100000 WHERE option_id=18 AND npc_option_npcflag=16;
--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1343,7 +1343,7 @@ public:
             case GOSSIP_OPTION_LEARNDUALSPEC:
                 if (player->GetSpecsCount() == 1 && !(player->getLevel() < sWorld->getIntConfig(CONFIG_MIN_DUALSPEC_LEVEL)))
                 {
-                    if (!player->HasEnoughMoney(10000000))
+                    if (!player->HasEnoughMoney(100000))
                     {
                         player->SendBuyError(BUY_ERR_NOT_ENOUGHT_MONEY, 0, 0, 0);
                         player->PlayerTalkClass->SendCloseGossip();
@@ -1351,7 +1351,7 @@ public:
                     }
                     else
                     {
-                        player->ModifyMoney(-10000000);
+                        player->ModifyMoney(-100000);
 
                         // Cast spells that teach dual spec
                         // Both are also ImplicitTarget self and must be cast by player
