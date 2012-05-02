51b8ab57d0d5aecf8d5a528c2a3c9002c46dd890
Bootz <bootz@projectskyfire.org>
2012-05-02 00:18:29 -0500

    Core/Spell: Fix glyph spell (57927)
    thanks to: Thema89
    
    this fixes Issue: #331

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1187,12 +1187,13 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                 // Todo: This should be moved to similar function in spell::hit
                 else if (GetSpellInfo()->SpellFamilyFlags[0] & 0x01000000)
                 {
-                    // Polymorph Sound - Sheep && Penguin
+                    // Polymorph Sound - Sheep, Penguin and Monkey
                     if (GetSpellInfo()->SpellIconID == 82 && GetSpellInfo()->SpellVisual[0] == 12978)
                     {
-                        // Glyph of the Penguin
-                        if (caster->HasAura(52648))
+                        if (caster->HasAura(52648))                 // Glyph of the Penguin
                             caster->CastSpell(target, 61635, true);
+                        else if (caster->HasAura(57927))            // Glyph of the Monkey
+                                 caster->CastSpell(target, 89729, true);
                         else
                             caster->CastSpell(target, 61634, true);
                     }
