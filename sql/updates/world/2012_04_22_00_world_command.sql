4ba91173bb848419bce33ca95d944e8191741a02
raydeon <stengray@hotmail.com>
2012-05-02 21:11:05 -0600

    Archived world sql updates and removed core scripting for a few npcs (which are now SAI scripted) to realign with Database Release 6.1

--- sql/updates/world/2012_04_22_00_world_command.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM command WHERE name='reload item_template_addon';
-INSERT INTO `command` VALUES ('reload item_template_addon', '3', 'Syntax: .reload item_template_addon\n\nReloads item_template_addon definitions from the database.');
