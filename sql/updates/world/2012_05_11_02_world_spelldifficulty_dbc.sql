ad58cba393f860cb30d783be4535ca465e651c0d
Bootz <bootz@projectskyfire.org>
2012-05-11 15:42:18 -0500

    DB: multiple fixes;
    * Updated some missing spelldifficulty_dbcs.
    * Added missing graveyards in a few places.
    * Corrected coords on some of our areatrigger_teleports, and added some missing.

---
+++ sql/updates/world/2012_05_11_02_world_spelldifficulty_dbc.sql
@@ -0,0 +1,8 @@
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (12202,42636,0,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (12203,45433,0,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13251,90120,92943,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13255,90059,92948,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13258,89289,92951,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13523,75621,94991,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13579,75708,95858,0,0);
+insert  into `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) values (13582,92666,95884,0,0);
---
+++ sql/updates/world/2012_05_11_04_world_areatrigger_teleport.sql
@@ -0,0 +1,35 @@
+-- Areatrigger_teleport fixed coords
+REPLACE  INTO `areatrigger_teleport` VALUES (4409,'Eastern Plaguelands To Sunstrider Isle',530,6127.64,-7006.57,139.045,5.95614);
+REPLACE  INTO `areatrigger_teleport` VALUES (322,'Gnomeregan Instance Start',0,-5142.78,894.675,287.863,5.36474);
+REPLACE  INTO `areatrigger_teleport` VALUES (525,'Gnomeregan Train Depot Instance',0,-4915.15,807.528,270.914,4.60716);
+REPLACE  INTO `areatrigger_teleport` VALUES (4386,'Sunstrider Isle to Eastern Plaguelands',0,3460.79,-4464.65,136.614,2.25566);
+REPLACE  INTO `areatrigger_teleport` VALUES (4399,'Sethekk Halls (Exit)',530,-3361.96,4660.41,-101.048,1.65638);
+REPLACE  INTO `areatrigger_teleport` VALUES (4403,'Auchenai Crypts (Exit)',530,-3362.22,5225.77,-101.049,4.67224);
+REPLACE  INTO `areatrigger_teleport` VALUES (4008,'Ruins Of Ahn\'Qiraj (Outside)',509,-8429.74,1512.14,31.9074,2.58);  
+REPLACE  INTO `areatrigger_teleport` VALUES (3930,'Zul\'Gurub Exit ',0,-11916.3,-1208.37,92.2868,1.61792);
+REPLACE  INTO `areatrigger_teleport` VALUES (3186,'Dire Maul',429,-62.9658,159.867,-3.46206,3.14788);
+REPLACE  INTO `areatrigger_teleport` VALUES (3126,'Maraudon',1,-1186.98,2875.95,85.7258,1.78443);
+REPLACE  INTO `areatrigger_teleport` VALUES (2568,'Scholomance Instance',0,1275.05,-2552.03,90.3994,3.6631);
+REPLACE  INTO `areatrigger_teleport` VALUES (2221,'Stratholme - Eastern Plaguelands Instance (Inside)',0,3235.46,-4050.6,108.45,1.93522);
+REPLACE  INTO `areatrigger_teleport` VALUES (1470,'Blackrock Spire - Searing Gorge Instance',0,-7524.19,-1230.13,285.743,2.09544);
+REPLACE  INTO `areatrigger_teleport` VALUES (119,'DeadMines Instance Start',0,-11208.3,1672.52,24.66,4.55217);
+REPLACE  INTO `areatrigger_teleport` VALUES (1472,'Blackrock Dephts - Searing Gorge Instance',0,-7179.63,-923.667,166.416,1.84097);
+REPLACE  INTO `areatrigger_teleport` VALUES (882,'Uldaman Instance End',0,-6620.48,-3765.19,266.226,3.13531);
+REPLACE  INTO `areatrigger_teleport` VALUES (6197,'Throne of the Tides(Exit)',0,-5599.09,5412.06,-1798.15,2.40455);
+REPLACE  INTO `areatrigger_teleport` VALUES (448,'Altar Of Atal\'Hakkar Instance Start',0,-10175.1,-3995.15,-112.9,2.95938);
+REPLACE  INTO `areatrigger_teleport` VALUES (503,'Stockades Instance',0,-8764.83,846.075,87.4842,3.77934);
+REPLACE  INTO `areatrigger_teleport` VALUES (444,'Razorfen Downs Instance Start',1,-4658.12,-2526.35,81.492,1.25978);
+REPLACE  INTO `areatrigger_teleport` VALUES (288,'Uldaman Instance Start',0,-6066.73,-2955.63,209.776,3.20443);
+REPLACE  INTO `areatrigger_teleport` VALUES (259,'Blackphantom Deeps Instance Start',1,4247.74,745.879,-24.5299,4.5828);
+
+-- new areatrigger_teleports 
+INSERT  INTO `areatrigger_teleport` VALUES (5931,'TheCrucibleofFlame (Enterance)',1,4679.1,-3675.53,696.452,1.3644);
+-- uncomment for 4.3x core/db
+-- insert  into `areatrigger_teleport` values (5880,'FirelandsForgeworks (Enterance)',1,5029.45,-2029.36,1148.97,0.214575);
+-- insert  into `areatrigger_teleport` values (5879,'FirelandsForgeworks (Exit)',1,5036.41,-2045.99,1368.39,5.2961);
+INSERT  INTO `areatrigger_teleport` VALUES (5937,'TheCrucibleofFlame (Exit)',1,4653.99,-3688.44,955.265,2.38149);
+INSERT  INTO `areatrigger_teleport` VALUES (5718,'Frozen Throne ICC (Entrance)',631,529.3,-2124.7,1050,3.14159);
+INSERT  INTO `areatrigger_teleport` VALUES (5700,'Deathbringer Spire Exit',631,-482.01,2211.44,541.2,3.14);
+INSERT  INTO `areatrigger_teleport` VALUES (5698,'Deathbringer Spire Entrance',631,4138.76,2769.25,351,0);
+
+
