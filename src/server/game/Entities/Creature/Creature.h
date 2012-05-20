7d85acf6857aa16533adbbe9264e30987b9565b2
SkyFire <admin@projectskyfire.org>
2012-05-19 20:56:17 -0500

    Revert "Implemented new scripting function. "SERVERSIDE_VISIBILITY_SUMMONER_ONLY"  Will add the SmartAI hook soon. For now it's for hard coded scripts. + Cleanup of duplicate shit."
    This reverts commit 4569e08fdd1975bf5a79ebdbc0d6d8f4827e135e.
    
    Will Work on this locally.
    
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -55,7 +55,6 @@ enum CreatureFlagsExtra
     CREATURE_FLAG_EXTRA_NO_SKILLGAIN    = 0x00040000,       // creature won't increase weapon skills
     CREATURE_FLAG_EXTRA_TAUNT_DIMINISH  = 0x00080000,       // Taunt is a subject to diminishing returns on this creautre
     CREATURE_FLAG_EXTRA_ALL_DIMINISH    = 0x00100000,       // Creature is subject to all diminishing returns as player are
-    CREATURE_FLAG_EXTRA_SUMMONER_ONLY   = 0x00200000,       // Creature can be seen by summoner only
     CREATURE_FLAG_EXTRA_DUNGEON_BOSS    = 0x10000000,       // creature is a dungeon boss (SET DYNAMICALLY, DO NOT ADD IN DB)
 };
 
@@ -64,7 +63,7 @@ enum CreatureFlagsExtra
     CREATURE_FLAG_EXTRA_NO_CRUSH | CREATURE_FLAG_EXTRA_NO_XP_AT_KILL | CREATURE_FLAG_EXTRA_TRIGGER | \
     CREATURE_FLAG_EXTRA_NO_TAUNT | CREATURE_FLAG_EXTRA_WORLDEVENT | CREATURE_FLAG_EXTRA_NO_CRIT | \
     CREATURE_FLAG_EXTRA_NO_SKILLGAIN | CREATURE_FLAG_EXTRA_TAUNT_DIMINISH | CREATURE_FLAG_EXTRA_ALL_DIMINISH | \
-    CREATURE_FLAG_EXTRA_GUARD | CREATURE_FLAG_EXTRA_SUMMONER_ONLY)
+    CREATURE_FLAG_EXTRA_GUARD)
 
 // GCC have alternative #pragma pack(N) syntax and old gcc version not support pack(push, N), also any gcc version not support it at some platform
 #if defined(__GNUC__)
@@ -470,7 +469,6 @@ class Creature : public Unit, public GridObject<Creature>, public MapCreature
         bool isGuard() const { return GetCreatureTemplate()->flags_extra & CREATURE_FLAG_EXTRA_GUARD; }
         bool canWalk() const { return GetCreatureTemplate()->InhabitType & INHABIT_GROUND; }
         bool canSwim() const { return GetCreatureTemplate()->InhabitType & INHABIT_WATER; }
-        bool isSummonerOnly() const { return GetCreatureTemplate()->flags_extra & CREATURE_FLAG_EXTRA_SUMMONER_ONLY; }
         //bool canFly()  const { return GetCreatureTemplate()->InhabitType & INHABIT_AIR; }
 
         void SetReactState(ReactStates st) { _reactState = st; }
