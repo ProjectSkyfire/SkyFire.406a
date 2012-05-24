15028c0e857ae2471b18d4846ecfd9034a356215
Greymane <rubenmussi@hotmail.com>
2012-05-24 16:47:38 -0300

    Core/Spell: Added missing proc event sql on last commit, lol.

--- sql/updates/world/2012_05_24_00_world_spell_proc_event.sql
+++ sql/updates/world/2012_05_24_00_world_spell_proc_event.sql
@@ -6,6 +6,12 @@ INSERT INTO `spell_linked_spell` VALUES
 -- On retail, you lose the 79683 aura by just clicking on arcane missiles
 (5143,-79683,0,'Arcane missiles! aura removal due to arcane missiles cast');
 
+-- 79683 in fact doesnt proc at all by using 5143, thats wy i remove the 79683 manually up there
+-- this is here just to make sure no other spell will proc it. 
+DELETE FROM `spell_proc_event` WHERE `entry` = 79683;
+INSERT INTO `spell_proc_event` VALUES
+(79683,64,3,2048,0,0,0,0,0,0,0);
+
 -- 79684 Offensive State (DND) for goblins and worgens
 DELETE FROM `playercreateinfo_spell` WHERE `race` IN (9,22) AND `class` = 8 AND `Spell` = 79684; 
 INSERT INTO `playercreateinfo_spell` VALUES
