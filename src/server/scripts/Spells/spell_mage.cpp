1f7c99076e3f4502e061ebb42fafc513335aa4f3
Greymane <rubenmussi@hotmail.com>
2012-05-28 15:02:16 -0300

    Core/Spells: Started fixing DB spellscript/aurascript startup errors due to Effect/Aura DBC changes of spells in the wotlk -> cata transgression, some are old as fuck and will be removed, some will just need some hook changes and effect renaming here and there.

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -119,62 +119,6 @@ public:
     }
 };
 
-enum SilvermoonPolymorph
-{
-    NPC_AUROSALIA   = 18744,
-};
-
-// TODO: move out of here and rename - not a mage spell
-class spell_mage_polymorph_cast_visual : public SpellScriptLoader
-{
-public:
-    spell_mage_polymorph_cast_visual() : SpellScriptLoader("spell_mage_polymorph_visual") { }
-
-    class spell_mage_polymorph_cast_visual_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(spell_mage_polymorph_cast_visual_SpellScript);
-
-        static const uint32 PolymorhForms[6];
-
-        bool Validate(SpellInfo const* /*spellEntry*/)
-        {
-            // check if spell ids exist in dbc
-            for (uint32 i = 0; i < 6; i++)
-                if (!sSpellMgr->GetSpellInfo(PolymorhForms[i]))
-                    return false;
-            return true;
-        }
-
-        void HandleDummy(SpellEffIndex /*effIndex*/)
-        {
-            if (Unit* target = GetCaster()->FindNearestCreature(NPC_AUROSALIA, 30.0f))
-                if (target->GetTypeId() == TYPEID_UNIT)
-                    target->CastSpell(target, PolymorhForms[urand(0, 5)], true);
-        }
-
-        void Register()
-        {
-            // add dummy effect spell handler to Polymorph visual
-            OnEffectHitTarget += SpellEffectFn(spell_mage_polymorph_cast_visual_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
-        }
-    };
-
-    SpellScript* GetSpellScript() const
-    {
-        return new spell_mage_polymorph_cast_visual_SpellScript();
-    }
-};
-
-const uint32 spell_mage_polymorph_cast_visual::spell_mage_polymorph_cast_visual_SpellScript::PolymorhForms[6] =
-{
-    SPELL_MAGE_SQUIRREL_FORM,
-    SPELL_MAGE_GIRAFFE_FORM,
-    SPELL_MAGE_SERPENT_FORM,
-    SPELL_MAGE_DRAGONHAWK_FORM,
-    SPELL_MAGE_WORGEN_FORM,
-    SPELL_MAGE_SHEEP_FORM
-};
-
 class spell_mage_summon_water_elemental : public SpellScriptLoader
 {
 public:
@@ -451,7 +395,6 @@ void AddSC_mage_spell_scripts()
     new spell_mage_frost_warding_trigger();
     new spell_mage_incanters_absorbtion_absorb();
     new spell_mage_incanters_absorbtion_manashield();
-    new spell_mage_polymorph_cast_visual;
     new spell_mage_summon_water_elemental;
     new spell_mage_ice_barrier;
 }
