3faf0a9ed134abfc5cd33463eb9e3794da9dde15
Bootz <bootz@projectskyfire.org>
2012-06-08 00:02:28 -0500

    Scripts/EasternKingdoms: Pulled a boss out that does not exist in cata. (old script)
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -148,7 +148,6 @@ void AddSC_boss_chimaeron();
 void AddSC_instance_blackwing_descent();
 void AddSC_deadmines();                      //Deadmines
 void AddSC_instance_deadmines();
-void AddSC_boss_mr_smite();
 void AddSC_boss_admiral_ripsnarl();
 void AddSC_boss_captain_cookie();
 void AddSC_boss_foe_reaper_5000();
@@ -833,8 +832,6 @@ void AddEasternKingdomsScripts()
     AddSC_boss_chimaeron();
     AddSC_instance_blackwing_descent();
     AddSC_deadmines();                      //Deadmines
-    AddSC_instance_deadmines();
-    AddSC_boss_mr_smite();
     AddSC_boss_admiral_ripsnarl();
     AddSC_boss_captain_cookie();
     AddSC_boss_foe_reaper_5000();
