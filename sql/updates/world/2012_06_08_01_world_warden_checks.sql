b4d4dda235b06cb4aa7243af1559f56723ca9c7c
Bootz <bootz@projectskyfire.org>
2012-06-09 18:01:21 -0500

    db: Sql correction, replaced upper casing.

--- sql/updates/world/2012_06_08_01_world_warden_checks.sql
+++ sql/updates/world/2012_06_08_01_world_warden_checks.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS `warden_checks`;
 
 CREATE TABLE `warden_checks` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `type` tinyint(3) unsigned DEFAULT NULL,
-  `data` varchar(48) DEFAULT NULL,
-  `str` varchar(20) DEFAULT NULL,
-  `address` int(10) unsigned DEFAULT NULL,
-  `length` tinyint(3) unsigned DEFAULT NULL,
-  `result` varchar(24) DEFAULT NULL,
-  `comment` varchar(50) DEFAULT NULL,
+  `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type` TINYINT(3) UNSIGNED DEFAULT NULL,
+  `data` VARCHAR(48) DEFAULT NULL,
+  `str` VARCHAR(20) DEFAULT NULL,
+  `address` INT(10) UNSIGNED DEFAULT NULL,
+  `length` TINYINT(3) UNSIGNED DEFAULT NULL,
+  `result` VARCHAR(24) DEFAULT NULL,
+  `comment` VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 
 INSERT INTO `warden_checks`(`id`,`type`,`data`,`str`,`address`,`length`,`result`,`comment`) VALUES 
