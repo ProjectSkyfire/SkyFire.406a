25d89d7e40276ddf96251154d85885b01d1d80b9
Bootz <bootz@projectskyfire.org>
2012-06-09 05:42:13 -0500

    DB: Stopped abnormal movements of (28408, 30273, 30268).

---
+++ sql/updates/world/2012_06_08_11_world_creature_template.sql
@@ -0,0 +1,5 @@
+--  Webbed Crusader - Stopped Movement --
+UPDATE `creature_template` SET `unit_flags` = 131076 WHERE `entry` IN (30273,30268);
+
+--  Primordial Drake Egg - Stopped Movement --
+UPDATE `creature_template` SET `unit_flags` = 131076 WHERE `entry` = 28408;
