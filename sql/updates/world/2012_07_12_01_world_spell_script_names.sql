50463bd3296a8d3ec87a1ff90e379a5672601eb1
bootz <bootz@projectskyfire.org>
2012-07-13 01:43:10 -0500

    Fixed Sql for last commit. thanks to walkum
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- sql/updates/world/2012_07_12_01_world_spell_script_names.sql
+++ sql/updates/world/2012_07_12_01_world_spell_script_names.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` = 77478
+DELETE FROM `spell_script_names` WHERE `spell_id` = 77478;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (77478, 'spell_sha_earthquake');
