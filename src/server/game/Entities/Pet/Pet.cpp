36b07fd1544a97315527003faf36e9285c2150a4
bootz <bootz@projectskyfire.org>
2012-07-15 09:20:09 -0500

    Core/Pets: Removed more useless bloodworm code, bloodworms were R.I.P as of 4.0.3
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -933,12 +933,6 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                     SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel * 4 + petlevel));
                     break;
                 }
-                case 28017: // Bloodworms
-                {
-                    SetCreateHealth(4 * petlevel);
-                    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel - 30 - (petlevel / 4)) + _owner->GetTotalAttackPowerValue(BASE_ATTACK) * 0.006f);
-                    SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel - 30 + (petlevel / 4)) + _owner->GetTotalAttackPowerValue(BASE_ATTACK) * 0.006f);
-                }
                 case 19668: // Shadowfiend
                 {
                     if (!pInfo)
