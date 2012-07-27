d385a6417b8390979ac2226e416ca74bc669ce83
bootz <bootz@projectskyfire.org>
2012-07-27 12:54:41 -0500

    Script/Spells: Fixed mage Cauterize.
    thanks to CataProject

---
+++ sql/updates/world/2012_07_27_00_world_spell_script_names.sql
@@ -0,0 +1,2 @@
+REPLACE INTO spell_script_names VALUES (-86948,'spell_mage_cauterize'); -- Cauterize rank 1
+REPLACE INTO spell_script_names VALUES (-86949,'spell_mage_cauterize'); -- Cauterize rank 2
