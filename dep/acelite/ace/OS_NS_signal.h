96b0ae63e4cb0c6fa71a019dc1c8e5a00c638d62
bootz <bootz@projectskyfire.org>
2012-08-02 10:15:35 -0500

    Dep/Ace: Apply ACE patches for FreeBSD.
    Shared/Crypt: Fixed an OpenSSL bug in the ARC4 stream cipher initialization
    that causes crashes on some OS's.  (temp solution.)
    (Mangos-port, credits to: antiroot & undergarun)
    
    - had to make a new vm, for F.Bsd :/
    if anyone else has better solution for ACR4 fix my ears are open.)

--- dep/acelite/ace/OS_NS_signal.h
+++ dep/acelite/ace/OS_NS_signal.h
@@ -33,10 +33,10 @@
 #endif
 #define ACE_EXPORT_MACRO ACE_Export
 
-#if defined (__Lynx__) || defined (__OpenBSD__)
-// LynxOS and OpenBSD define pthread_sigmask() in pthread.h
+#if defined (__Lynx__) || defined (__OpenBSD__) || defined (__FreeBSD__)
+// LynxOS, OpenBSD and FreeBSD define pthread_sigmask() in pthread.h
 # include "ace/os_include/os_pthread.h"
-#endif /* __Lynx__ || OpenBSD */
+#endif /* __Lynx__ || OpenBSD || FreeBSD */
 
 /*
  * We inline and undef some functions that may be implemented
