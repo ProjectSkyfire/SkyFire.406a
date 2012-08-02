bf97a30dd822bad6dbe0bd12b1502578ad78653e
bootz <bootz@projectskyfire.org>
2012-08-02 16:18:57 -0500

    Correctly fix commit ebe9ae758dc6261177b5dc0e9b391ef20a7acaef for vs.2012 (preview/professional)
    - this also fixes the vs issue, where 10 would get over-ran by 12 as native, during generation.
    - tested under all three visual studios (9/10/11-12).

--- src/server/shared/Dynamic/UnorderedMap.h
+++ src/server/shared/Dynamic/UnorderedMap.h
@@ -23,40 +23,97 @@
 #include "Define.h"
 
 #if COMPILER == COMPILER_INTEL
-#include <ext/hash_map>
+#  include <ext/hash_map>
+#  include <ext/hash_set>
 #elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
-#include <tr1/unordered_map>
+#  include <tr1/unordered_map>
+#  include <tr1/unordered_set>
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
-#include <ext/hash_map>
-#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1    // VC9.0 and later
-#include <unordered_map>
+#  include <ext/hash_map>
+#  include <ext/hash_set>
+#elif COMPILER == COMPILER_MICROSOFT && (_MSC_VER > 1500 || _MSC_VER == 1500 && _HAS_TR1)   // VC9.0 SP1 and later
+#  include <unordered_map>
+#  include <unordered_set>
 #else
-#include <hash_map>
+#  include <hash_map>
+#  include <hash_set>
 #endif
 
 #ifdef _STLPORT_VERSION
-#define UNORDERED_MAP std::hash_map
+#  define UNORDERED_MAP std::hash_map
+#  define UNORDERED_SET std::hash_set
+#  define HASH_NAMESPACE_START namespace std {
+#  define HASH_NAMESPACE_END }
 using std::hash_map;
+using std::hash_set;
+#elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1600    // VS100
+#  define UNORDERED_MAP std::unordered_map
+#  define UNORDERED_SET std::unordered_set
+#  define HASH_NAMESPACE_START namespace std {
+#  define HASH_NAMESPACE_END }
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1
-#define UNORDERED_MAP std::tr1::unordered_map
+#  define UNORDERED_MAP std::tr1::unordered_map
+#  define UNORDERED_SET std::tr1::unordered_set
+#  define HASH_NAMESPACE_START namespace std { namespace tr1 {
+#  define HASH_NAMESPACE_END } }
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1300
-#define UNORDERED_MAP stdext::hash_map
+#  define UNORDERED_MAP stdext::hash_map
+#  define UNORDERED_SET stdext::hash_set
+#  define HASH_NAMESPACE_START namespace stdext {
+#  define HASH_NAMESPACE_END }
 using stdext::hash_map;
-#elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
-#define UNORDERED_MAP std::tr1::unordered_map
-#elif (COMPILER == COMPILER_GNU && __GNUC__ >= 3) || COMPILER == COMPILER_INTEL
-#define UNORDERED_MAP __gnu_cxx::hash_map
+using stdext::hash_set;
 
-namespace __gnu_cxx
+#if !_HAS_TRADITIONAL_STL
+
+// can be not used by some platforms, so provide fake forward
+HASH_NAMESPACE_START
+
+template<class K>
+class hash
 {
-    template<> struct hash<unsigned long long>
+    public:
+        size_t operator() (K const&);
+};
+
+HASH_NAMESPACE_END
+
+#endif
+
+#elif COMPILER == COMPILER_INTEL
+#  define UNORDERED_MAP std::hash_map
+#  define UNORDERED_SET std::hash_set
+#  define HASH_NAMESPACE_START namespace std {
+#  define HASH_NAMESPACE_END }
+using std::hash_map;
+using std::hash_set;
+#elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
+#  define UNORDERED_MAP std::tr1::unordered_map
+#  define UNORDERED_SET std::tr1::unordered_set
+#  define HASH_NAMESPACE_START namespace std { namespace tr1 {
+#  define HASH_NAMESPACE_END } }
+#elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
+#  define UNORDERED_MAP __gnu_cxx::hash_map
+#  define UNORDERED_SET __gnu_cxx::hash_set
+#  define HASH_NAMESPACE_START namespace __gnu_cxx {
+#  define HASH_NAMESPACE_END }
+
+HASH_NAMESPACE_START
+
+    template<>
+    class hash<unsigned long long>
     {
-        size_t operator()(const unsigned long long &__x) const { return (size_t)__x; }
+        public:
+            size_t operator()(const unsigned long long &__x) const { return (size_t)__x; }
     };
-    template<typename T> struct hash<T *>
+
+    template<typename T>
+    class hash<T *>
     {
-        size_t operator()(T * const &__x) const { return (size_t)__x; }
+        public:
+            size_t operator()(T * const &__x) const { return (size_t)__x; }
     };
+
     template<> struct hash<std::string>
     {
         size_t operator()(const std::string &__x) const
@@ -64,10 +121,16 @@ namespace __gnu_cxx
             return hash<const char *>()(__x.c_str());
         }
     };
-};
+
+HASH_NAMESPACE_END
 
 #else
-#define UNORDERED_MAP std::hash_map
+#  define UNORDERED_MAP std::hash_map
+#  define UNORDERED_SET std::hash_set
+#  define HASH_NAMESPACE_START namespace std {
+#  define HASH_NAMESPACE_END }
 using std::hash_map;
+using std::hash_set;
 #endif
+
 #endif
