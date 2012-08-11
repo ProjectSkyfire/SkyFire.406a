5c8ee1782caf79b001de9ea3db8b32c93f0a0224
Kraven5 <git@github.com:Arkania/ArkCORE-4.3.4.git>
2012-08-11 04:53:07 -0400

    Fix Temple of Earth sanctuary

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7745,7 +7745,7 @@ void Player::UpdateArea(uint32 newArea)
 
     // previously this was in UpdateZone (but after UpdateArea) so nothing will break
     pvpInfo.inNoPvPArea = false;
-    if (area && area->IsSanctuary())    // in sanctuary
+    if ((area && area->IsSanctuary()) || area->ID == 5303)    // Temple of Earth sanctuary
     {
         SetByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SANCTUARY);
         pvpInfo.inNoPvPArea = true;
