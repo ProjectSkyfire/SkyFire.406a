15b695abaa16ba12fe90bc3b053a743353620ceb
SkyFire <admin@projectskyfire.org>
2012-11-27 18:59:31 -0600

    Looks like I borked the VS 10/11 script. This should fix it.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- contrib/Tools/AutoScripts/Project_Build_Tool.bat
+++ contrib/Tools/AutoScripts/Project_Build_Tool.bat
@@ -25,7 +25,7 @@ REM     Make Sure you Edit config feature's for your specific build.
 REM     this fixes native pre-builds for VC10 (MS Visual studio 10 Pro)
 REM     and VC11 (MS Visual studio 11 or 2012 Pro/Ultimate)
 REM     Warning! VS Express edition is not supported!
-SET compiler=VC9
+SET compiler=VC10
 REM Install path for SkyFireEMU (in this be created ./bin and ./etc folders)
 SET INSTALL_PATH="C:\\GAMES\\SkyFireEMU"
 rem
@@ -34,7 +34,7 @@ rem     Options: Win32, Win64. (Warning! Win64 build possible only on 64-bit mai
 SET BUILD_PLATFORM=Win64
 rem
 rem This Setting is For Multi-Core Processor configurations(core Count)for speedup only
-SET CORE_NUMBER=1
+SET CORE_NUMBER=4
 rem
 rem Build map extractors. 1=Yes or 0=No.
 SET TOOLS=1
@@ -43,7 +43,6 @@ rem "TODO: Add-ins for Memory Manager(s)"
 REM *****************************************************************************************
 if %compiler%==VC11 goto :vc11
 if %compiler%==VC10 goto :vc10
-if %compiler%==VC9 goto :vc9
 goto :help
 REM *****************************************************************************************
 :vc11
@@ -51,9 +50,9 @@ SET COMPILER="Visual Studio 11"
 if %BUILD_PLATFORM%==Win64 (SET COMPILER="Visual Studio 11 Win64")
 if %TOOLS%==0 (SET TOOL="")
 if %TOOLS%==1 (SET TOOL="-DTOOLS=%INSTALL_PATH%")
-SET COMPILER_PATH="C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin/cl.exe"
-SET LINKER_PATH="C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin/link.exe"
-SET VC_VARS="C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\"
+SET COMPILER_PATH="C:/Program Files/Microsoft Visual Studio 11.0/VC/bin/cl.exe"
+SET LINKER_PATH="C:/Program Files/Microsoft Visual Studio 11.0/VC/bin/link.exe"
+SET VC_VARS="C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\"
 goto :common
 REM *****************************************************************************************
 :vc10
@@ -64,16 +63,6 @@ if %TOOLS%==1 (SET TOOL="-DTOOLS=%INSTALL_PATH%")
 SET COMPILER_PATH="C:/Program Files/Microsoft Visual Studio 10.0/VC/bin/cl.exe"
 SET LINKER_PATH="C:/Program Files/Microsoft Visual Studio 10.0/VC/bin/link.exe"
 SET VC_VARS="C:\\Program Files\\Microsoft Visual Studio 10.0\\VC\\"
-goto :common
-REM *****************************************************************************************
-:vc9
-SET COMPILER="Visual Studio 9 2008"
-if %BUILD_PLATFORM%==Win64 (SET COMPILER="Visual Studio 9 2008 Win64")
-if %TOOLS%==0 (SET TOOL="")
-if %TOOLS%==1 (SET TOOL="-DTOOLS=%INSTALL_PATH%")
-SET COMPILER_PATH="C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/cl.exe"
-SET LINKER_PATH="C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/link.exe"
-SET VC_VARS="C:\\Program Files (x86)\\Microsoft Visual Studio 9.0\\VC\\"
 goto :common
 REM *****************************************************************************************
 :help
@@ -108,14 +97,14 @@ REM ****************************************************************************
 cd build
 cmake -G %COMPILER% -DPCH=1 -DCMAKE_CXX_COMPILER=%COMPILER_PATH% -DCMAKE_CXX_FLAGS=%C_FLAGS% -DCMAKE_C_FLAGS=%C_FLAGS% -DCMAKE_CXX_COMPILER=%COMPILER_PATH% -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH% %TOOL% ..
 call %VC_VARS%vcvarsall.bat
-MSBuild SkyFireEMU.sln /m:%CORE_NUMBER% /t:Rebuild /p:Configuration=%RESULT_CONF%;Platform=%BUILD_PLATFORM%
+MSBuild INSTALL.vcxproj /m:%CORE_NUMBER% /t:Rebuild /p:Configuration=%RESULT_CONF%;Platform=%BUILD_PLATFORM%
 goto :end
 REM *****************************************************************************************
 :win64
 cd build
 cmake -G %COMPILER% -DPCH=1 -DPLATFORM=X64 -DCMAKE_CXX_FLAGS=%C_FLAGS% -DCMAKE_C_FLAGS=%C_FLAGS% -DCMAKE_CXX_COMPILER=%COMPILER_PATH% -DCMAKE_CXX_COMPILER=%COMPILER_PATH% -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH% %TOOL% ..
 call %VC_VARS%vcvarsall.bat
-MSBuild SkyFireEMU.sln /m:%CORE_NUMBER%  /t:Rebuild /p:Configuration=%RESULT_CONF%;Platform=x64
+MSBuild INSTALL.vcxproj /m:%CORE_NUMBER%  /t:Rebuild /p:Configuration=%RESULT_CONF%;Platform=x64
 goto :end
 REM *****************************************************************************************
 
