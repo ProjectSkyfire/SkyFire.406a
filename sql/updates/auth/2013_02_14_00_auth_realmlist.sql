f11194f524bb11391fde103f6050e52142dc5490
bootz <bootz@projectskyfire.org>
2013-02-13 07:14:37 -0600

    Revert that sql, this was not to go in yet. :s derp derp
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- sql/updates/auth/2013_02_14_00_auth_realmlist.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `realmlist`
-  ADD `localAddress` VARCHAR(255) NOT NULL DEFAULT '127.0.0.1' AFTER `address`,
-  ADD `localSubnetMask` VARCHAR(255) NOT NULL DEFAULT '255.255.255.0' AFTER `localAddress`;
-    
