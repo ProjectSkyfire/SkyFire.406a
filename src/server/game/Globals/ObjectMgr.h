bd999a0f1ac03b58a36ca5ab13b19c4445ce690e
SkyFire <admin@projectskyfire.org>
2013-02-14 10:52:10 -0600

    Core/Loading: Make GameObject loot loading faster by removing an unneeded call to an expensive method. Patch by Subv By suggestion from Manuel. https://github.com/TrinityCore/TrinityCore/commit/6400c13fcb342fff7e2d808e469f2c57d83766c5

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -1050,15 +1050,6 @@ class ObjectMgr
             return &itr->second;
         }
 
-        bool IsGoOfSpecificEntrySpawned(uint32 entry) const
-        {
-            for (GameObjectDataContainer::const_iterator it = _gameObjectDataStore.begin(); it != _gameObjectDataStore.end(); ++it)
-                if (it->second.id == entry)
-                    return true;
-
-            return false;
-        }
-
         GameObjectData const* GetGOData(uint32 guid) const
         {
             GameObjectDataContainer::const_iterator itr = _gameObjectDataStore.find(guid);
