710e6f77284d5b4c0922dd8448cf673879e0c5d3
SkyFire <admin@projectskyfire.org>
2013-02-27 22:22:47 -0600

    Core/Commands: Converted several other commands to scripts. Ban, Cast, Instance, Lookup, Reset, Ticket. Fixed another derp from previous commit. original patch by TC from various authors. Thx.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/Commands/cs_bf.cpp
+++ src/server/scripts/Commands/cs_bf.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2011-2013 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
