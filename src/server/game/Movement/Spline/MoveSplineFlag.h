1ffdff389fbd5f7bc7b7122ac2219614f0a57215
bootz <bootz@projectskyfire.org>
2013-03-16 01:49:32 -0500

    Core/Movement: Fixed Taxi's spline movements thanks to cyberbrest
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- src/server/game/Movement/Spline/MoveSplineFlag.h
+++ src/server/game/Movement/Spline/MoveSplineFlag.h
@@ -34,33 +34,34 @@ namespace Movement
     class MoveSplineFlag
     {
     public:
-        enum eFlags{
-            None         = 0x00000000,
-                                                 // x00-xFF(first byte) used as animation Ids storage in pair with Animation flag
-            Done         = 0x00000100,
-            Falling      = 0x00000200,           // Affects elevation computation, can't be combined with Parabolic flag
-            No_Spline    = 0x00000400,
-            Parabolic    = 0x00000800,           // Affects elevation computation, can't be combined with Falling flag
-            Walkmode     = 0x00001000,
-            Flying       = 0x00002000,           // Smooth movement(Catmullrom interpolation mode), flying animation
-            OrientationFixed = 0x00004000,       // Model orientation fixed
-            Final_Point  = 0x00008000,
-            Final_Target = 0x00010000,
-            Final_Angle  = 0x00020000,
-            Catmullrom   = 0x00040000,           // Used Catmullrom interpolation mode
-            Cyclic       = 0x00080000,           // Movement by cycled spline
-            Enter_Cycle  = 0x00100000,           // Everytimes appears with cyclic flag in monster move packet, erases first spline vertex after first cycle done
-            Animation    = 0x00200000,           // Plays animation after some time passed
-            Frozen       = 0x00400000,           // Will never arrive
-            Transport    = 0x00800000,
-            Exit_Vehicle = 0x01000000,
-            Unknown26     = 0x02000000,
-            Unknown27     = 0x04000000,
-            OrientationInversed = 0x08000000,
-            UsePathSmoothing    = 0x10000000,
-            Animation2    = 0x20000000,
-            UnCompressedPath    = 0x40000000,
-            Unknown32    = 0x80000000,
+        enum splineFlags
+        {
+            None					= 0x00000000,
+                                                            // x00-xFF(first byte) used as animation Ids storage in pair with Animation flag
+            Done                    = 0x00000100,
+            Falling                 = 0x00000200,           // Affects elevation computation, can't be combined with Parabolic flag
+            No_Spline               = 0x00000400,
+            Parabolic               = 0x00000800,           // Affects elevation computation, can't be combined with Falling flag
+            Walkmode                = 0x00001000,
+            Flying                  = 0x00002000,           // Smooth movement(Catmullrom interpolation mode), flying animation
+            OrientationFixed        = 0x00004000,			// Model orientation fixed
+            Final_Point             = 0x00008000,
+            Final_Target            = 0x00010000,
+            Final_Angle             = 0x00020000,
+            Catmullrom              = 0x00040000,           // Used Catmullrom interpolation mode
+            Cyclic                  = 0x00080000,           // Movement by cycled spline
+            Enter_Cycle             = 0x00100000,           // Everytime appears with cyclic flag in monster move packet, erases first spline vertex after first cycle done
+            Animation               = 0x00200000,           // Plays animation after some time passed
+            Frozen                  = 0x00400000,           // Will never arrive
+            Transport               = 0x00800000,
+            Exit_Vehicle            = 0x01000000,
+            Unknown26               = 0x02000000,
+            Unknown27               = 0x04000000,
+            OrientationInversed     = 0x08000000,
+            UsePathSmoothing        = 0x10000000,
+            Animation2              = 0x20000000,
+            UnCompressedPath        = 0x40000000,
+            Unknown32               = 0x80000000,
 
             // Masks
             Mask_Final_Facing = Final_Point | Final_Target | Final_Angle,
@@ -71,11 +72,11 @@ namespace Movement
             // CatmullRom interpolation mode used
             Mask_CatmullRom = Flying | Catmullrom,
             // Unused, not supported flags
-            Mask_Unused = No_Spline|Enter_Cycle|Frozen|Transport|Exit_Vehicle|Unknown26|Unknown27,
+            Mask_Unused = No_Spline | Enter_Cycle | Frozen | Transport | Exit_Vehicle | Unknown26 | Unknown27,
         };
 
-        inline uint32& raw() { return (uint32&)*this;}
-        inline const uint32& raw() const { return (const uint32&)*this;}
+        inline uint32& raw() { return (uint32&)*this; }
+        inline const uint32& raw() const { return (const uint32&)*this; }
 
         MoveSplineFlag() { raw() = 0; }
         MoveSplineFlag(uint32 f) { raw() = f; }
@@ -83,57 +84,59 @@ namespace Movement
 
         // Constant interface
 
-        bool isSmooth() const { return raw() & Mask_CatmullRom;}
-        bool isLinear() const { return !isSmooth();}
-        bool isFacing() const { return raw() & Mask_Final_Facing;}
+        bool isSmooth() const { return raw() & Mask_CatmullRom; }
+        bool isLinear() const { return !isSmooth(); }
+        bool isFacing() const { return raw() & Mask_Final_Facing; }
 
-        uint8 getAnimationId() const { return animId;}
-        bool hasAllFlags(uint32 f) const { return (raw() & f) == f;}
-        bool hasFlag(uint32 f) const { return (raw() & f) != 0;}
-        uint32 operator & (uint32 f) const { return (raw() & f);}
-        uint32 operator | (uint32 f) const { return (raw() | f);}
+        uint8 getAnimationId() const { return animId; }
+        bool hasAllFlags(uint32 f) const { return (raw() & f) == f; }
+        bool hasFlag(uint32 f) const { return (raw() & f) != 0; }
+        uint32 operator & (uint32 f) const { return (raw() & f); }
+        uint32 operator | (uint32 f) const { return (raw() | f); }
         std::string ToString() const;
 
         // Not constant interface
 
-        void operator &= (uint32 f) { raw() &= f;}
-        void operator |= (uint32 f) { raw() |= f;}
+        void operator &= (uint32 f) { raw() &= f; }
+        void operator |= (uint32 f) { raw() |= f; }
 
-        void EnableAnimation(uint8 anim) { raw() = raw() & ~(Mask_Animations|Falling|Parabolic) | Animation|anim;}
-        void EnableParabolic() { raw() = raw() & ~(Mask_Animations|Falling|Animation) | Parabolic;}
-        void EnableFalling() { raw() = raw() & ~(Mask_Animations|Parabolic|Animation) | Falling;}
-        void EnableFlying() { raw() = raw() & ~Catmullrom | Flying | UnCompressedPath | UsePathSmoothing | Unknown32 | Unknown26;}
-        void EnableCatmullRom() { raw() = raw() & ~Flying | Catmullrom | UnCompressedPath | UsePathSmoothing | Unknown32 | Unknown26;}
-        void EnableFacingPoint() { raw() = raw() & ~Mask_Final_Facing | Final_Point;}
-        void EnableFacingAngle() { raw() = raw() & ~Mask_Final_Facing | Final_Angle;}
-        void EnableFacingTarget() { raw() = raw() & ~Mask_Final_Facing | Final_Target;}
+        void EnableAnimation(uint8 anim) { raw() = raw() & ~(Mask_Animations | Falling | Parabolic) | Animation|anim; }
+        void EnableParabolic() { raw() = raw() & ~(Mask_Animations | Falling | Animation) | Parabolic; }
+        void EnableFalling() { raw() = raw() & ~(Mask_Animations | Parabolic | Animation) | Falling; }
+        void EnableFlying() { raw() = raw() & ~Catmullrom | Flying | UnCompressedPath | UsePathSmoothing | Unknown32 | Unknown26; }
+        void EnableCatmullRom() { raw() = raw() & ~Flying | Catmullrom | UnCompressedPath | UsePathSmoothing | Unknown32 | Unknown26; }
+        void EnableFacingPoint() { raw() = raw() & ~Mask_Final_Facing | Final_Point; }
+        void EnableFacingAngle() { raw() = raw() & ~Mask_Final_Facing | Final_Angle; }
+        void EnableFacingTarget() { raw() = (raw() & ~Mask_Final_Facing) | Final_Target; }
+        void EnableTaxiFlight() { raw() = raw() | Catmullrom | Flying | Walkmode | UnCompressedPath; }
 
-        uint8 animId       : 8;
-        bool done          : 1;
-        bool falling       : 1;
-        bool no_spline     : 1;
-        bool parabolic     : 1;
-        bool walkmode      : 1;
-        bool flying        : 1;
-        bool orientationFixed : 1;
-        bool final_point   : 1;
-        bool final_target  : 1;
-        bool final_angle   : 1;
-        bool catmullrom    : 1;
-        bool cyclic        : 1;
-        bool enter_cycle   : 1;
-        bool animation     : 1;
-        bool frozen        : 1;
-        bool transport     : 1;
-        bool exit_vehicle  : 1;
-        bool unknown26      : 1;
-        bool unknown27      : 1;
-        bool orientationInversed : 1;
-        bool usePathSmoothing    : 1;
-        bool animation2     : 1;
-        bool unCompressedPath    : 1;
-        bool unknown32     : 1;
+        uint8 animId				: 8;
+        bool done                   : 1;
+        bool falling                : 1;
+        bool no_spline              : 1;
+        bool parabolic              : 1;
+        bool walkmode               : 1;
+        bool flying                 : 1;
+        bool orientationFixed       : 1;
+        bool final_point            : 1;
+        bool final_target           : 1;
+        bool final_angle            : 1;
+        bool catmullrom             : 1;
+        bool cyclic                 : 1;
+        bool enter_cycle            : 1;
+        bool animation              : 1;
+        bool frozen                 : 1;
+        bool transport              : 1;
+        bool exit_vehicle           : 1;
+        bool unknown26              : 1;
+        bool unknown27              : 1;
+        bool orientationInversed    : 1;
+        bool usePathSmoothing       : 1;
+        bool animation2             : 1;
+        bool uncompressedPath       : 1;
+        bool unknown32              : 1;
     };
+
 #if defined( __GNUC__ )
 #pragma pack()
 #else
