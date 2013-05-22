c1f776a50a84cee1d8669de07a5113dbd858731d
bootz <bootz@projectskyfire.org>
2013-05-22 17:53:24 -0500

    typo fix lost a ;

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -849,7 +849,7 @@ void Item::RemoveFromUpdateQueueOf(Player* player)
     if (!IsInUpdateQueue())
         return;
 
-    ASSERT(player != NULL)
+    ASSERT(player != NULL);
 
     if (player->GetGUID() != GetOwnerGUID())
     {
