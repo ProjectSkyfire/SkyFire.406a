766c6938180989f8c348204e0d3c75a1e904864e
ImageDev <EmageDev@gmail.com>
2013-05-30 17:11:35 -0500

    Core/Debugging: Mark assert functions as non-returning. thanks to: Shauren
    Signed-off-by: ImageDev <EmageDev@gmail.com>

--- src/server/shared/Debugging/Errors.h
+++ src/server/shared/Debugging/Errors.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2011-2013 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
  *
@@ -19,15 +20,26 @@
 #ifndef SKYFIRE_ERRORS_H
 #define SKYFIRE_ERRORS_H
 
-#include "Common.h"
-#include "Log.h"
-#include <ace/Stack_Trace.h>
-#include <ace/OS_NS_unistd.h>
+#include "Define.h"
 
-#define WPAssert(assertion) { if (!(assertion)) { ACE_Stack_Trace st; sLog->outError("\n%s:%i in %s ASSERTION FAILED:\n  %s\n%s\n", __FILE__, __LINE__, __FUNCTION__, #assertion, st.c_str()); *((volatile int*)NULL) = 0; } }
-#define WPError(assertion, errmsg) { if (!(assertion)) { sLog->outError("%\n%s:%i in %s ERROR:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); *((volatile int*)NULL) = 0; } }
-#define WPWarning(assertion, errmsg) { if (!(assertion)) { sLog->outError("\n%s:%i in %s WARNING:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); } }
-#define WPFatal(assertion, errmsg) { if (!(assertion)) { sLog->outError("\n%s:%i in %s FATAL ERROR:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); ACE_OS::sleep(10); *((volatile int*)NULL) = 0; } }
+namespace SkyFire
+{
+
+    DECLSPEC_NORETURN void Assert(char const *file, int line, char const *function, char const *message) ATTR_NORETURN;
+
+    DECLSPEC_NORETURN void Fatal(char const *file, int line, char const *function, char const *message) ATTR_NORETURN;
+
+    DECLSPEC_NORETURN void Error(char const *file, int line, char const *function, char const *message) ATTR_NORETURN;
+
+    void Warning(char const *file, int line, char const *function, char const *message);
+
+} // namespace SkyFire
+
+#define WPAssert(cond) do { if (!(cond)) SkyFire::Assert(__FILE__, __LINE__, __FUNCTION__, #cond); } while(0)
+#define WPFatal(cond, msg) do { if (!(cond)) SkyFire::Fatal(__FILE__, __LINE__, __FUNCTION__, (msg)); } while(0)
+#define WPError(cond, msg) do { if (!(cond)) SkyFire::Error(__FILE__, __LINE__, __FUNCTION__, (msg)); } while(0)
+#define WPWarning(cond, msg) do { if (!(cond)) SkyFire::Warning(__FILE__, __LINE__, __FUNCTION__, (msg)); } while(0)
 
 #define ASSERT WPAssert
+
 #endif
