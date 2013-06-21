e9a2fd28cb1a78710fdac90985af9b3bded5e7dd
bootz <bootz@projectskyfire.org>
2013-06-21 17:45:20 -0500

    Tabs2spaces fix for last commit
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- src/server/shared/Common.h
+++ src/server/shared/Common.h
@@ -158,12 +158,12 @@ enum TimeConstants
 
 enum AccountTypes
 {
-	SEC_PLAYER          = 0,
-	SEC_MODERATOR       = 1,
-	SEC_GAMEMASTER      = 2,
+    SEC_PLAYER          = 0,
+    SEC_MODERATOR       = 1,
+    SEC_GAMEMASTER      = 2,
     SEC_DEVELOPER       = 3,
-	SEC_ADMINISTRATOR   = 4,
-	SEC_CONSOLE         = 5                    // must be always last in list, accounts must have less security level always also
+    SEC_ADMINISTRATOR   = 4,
+    SEC_CONSOLE         = 5                    // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
