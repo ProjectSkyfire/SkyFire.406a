a3515ee1866d2c61d23e8ab3aee950304a3d2131
bootz <bootz@projectskyfire.org>
2013-06-22 15:09:15 -0500

    db: after another check on itemloot, removed a bad entry
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- sql/updates/world/2013_22_06_00_world_misc.sql
+++ sql/updates/world/2013_22_06_00_world_misc.sql
@@ -7,11 +7,11 @@ UPDATE npc_trainer SET spell = 774, reqlevel = 8 WHERE entry = 3060 AND spell = 
 DELETE FROM creature_loot_template WHERE entry IN (9523,9524,3275,44170,3396,3397) AND item = 5030;
 INSERT INTO creature_loot_template VALUES
 (9523, 5030, -100, 1, 0, 1, 1),
-(9524, 5030, -93, 1, 0, 1, 1),
 (3275, 5030, -80, 1, 0, 1, 1),
-(44170, 5030, -39, 1, 0, 1, 1),
 (3396, 5030, -22, 1, 0, 1, 1),
-(3397, 5030, -11, 1, 0, 1, 1);
+(3397, 5030, -11, 1, 0, 1, 1)
+(44170, 5030, -39, 1, 0, 1, 1);
+
 
 -- Poison Water quest items drop fix
 DELETE FROM creature_loot_template WHERE entry IN (2958, 2956, 2957) AND item IN (4758, 4759);
