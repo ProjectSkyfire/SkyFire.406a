89537fd393fa1409231e23bd8eb639224c6a261e
bootz <bootz@projectskyfire.org>
2013-07-21 21:18:11 -0500

    DB: Script_texts for 2 BlackWing Descent Bosses.
    this is a particle merge of @F03SD Pull Request #912
    this adds Script_texts to Lord Victor Nefarius, and Omnotron
    of BWD.
    
    Signed-off-by: bootz <bootz@projectskyfire.org>

---
+++ sql/updates/world/2013_21_07_00_world_script_texts.sql
@@ -0,0 +1,17 @@
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc2`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+(42186,-2000005, "Toxitron unit shield system online.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000006, "Magmatron unit shield system online.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000007, "Electron unit shield system online.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000008, "Arcanotron unit shield system online.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000009, "Toxitron unit activated.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000010, "Magmatron unit activated.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000011, "Electron unit activated.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000012, "Arcanotron unit activated.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000013, "Rerouting excess energy to Magmatron unit. Engage flamethrower device.", NULL, 0, 14, 0, 0, NULL),
+(42186,-2000014, "Defense systems obliterated. Powering down....", NULL, 0, 14, 0, 0, NULL);
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc2`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+(41379,-2000000, "Hmm... the Omnotron Defense System. Centuries ago these constructs were considered the Dwarves greatest technical achievements. With so many counters to each construct's attacks. I'll have to rectify these designs for them to provide me any entertainement.", NULL, 0, 14, 0, 0, NULL),
+(41379,-2000001, "Stupid Dwarnes and you fascination with runes! Why would you create something that would help your enemy?", NULL, 0, 14, 0, 0, NULL),
+(41379,-2000002, "Lightning conductor works much better in a crowd, yes?", NULL, 0, 14, 0, 0, NULL),
+(41379,-2000003, "Were you planning on using Toxitron's chemical to damage the other constructs? Clever plan, let me ruin that for you.", NULL, 0, 14, 0, 0, NULL),
+(41379,-2000004, "You would like to move away from that, wouldn't you? Too bad.", NULL, 0, 14, 0, 0, NULL);
