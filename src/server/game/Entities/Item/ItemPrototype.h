b4c8dba946a8bf519e8a0f54db0fdeef0bc62f5a
Roc13x <roc13x@gmail.com>
2013-09-01 16:02:49 +0100

    Core/Items: Partial revert on last commit

--- src/server/game/Entities/Item/ItemPrototype.h
+++ src/server/game/Entities/Item/ItemPrototype.h
@@ -377,12 +377,11 @@ enum ItemSubclassArmor
     ITEM_SUBCLASS_ARMOR_LEATHER                 = 2,
     ITEM_SUBCLASS_ARMOR_MAIL                    = 3,
     ITEM_SUBCLASS_ARMOR_PLATE                   = 4,
-    ITEM_SUBCLASS_ARMOR_BUCKLER                 = 5, // OBSOLETE
     ITEM_SUBCLASS_ARMOR_SHIELD                  = 6,
-    ITEM_SUBCLASS_ARMOR_LIBRAM                  = 7,
-    ITEM_SUBCLASS_ARMOR_IDOL                    = 8,
-    ITEM_SUBCLASS_ARMOR_TOTEM                   = 9,
-    ITEM_SUBCLASS_ARMOR_SIGIL                   = 10,
+    //ITEM_SUBCLASS_ARMOR_LIBRAM                  = 7,
+    //ITEM_SUBCLASS_ARMOR_IDOL                    = 8,
+    //ITEM_SUBCLASS_ARMOR_TOTEM                   = 9,
+    //ITEM_SUBCLASS_ARMOR_SIGIL                   = 10,
     ITEM_SUBCLASS_ARMOR_RELIC                   = 11
 };
 
