2391510639028d227012d33e2e82d46747273ec9
SkyFire <admin@projectskyfire.org>
2013-10-16 11:46:58 -0500

    Removed the option of building without the example scrips.  So they always build now. It was just causing a headache in *nix.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -42,13 +42,6 @@ else()
   set(USE_SCRIPTPCH 0)
 endif()
 
-if( EXAMPLES )
-  message("* Build example scripts  : Yes")
-  add_definitions(-EXAMPLES)
-else()
-  message("* Build example scripts  : No (default)")
-endif()
-
 if( TOOLS )
   message("* Build map/vmap tools   : Yes")
 else()
