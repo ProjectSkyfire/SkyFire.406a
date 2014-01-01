90d5631a50e91e216c5bc32ed02a5693342e6827
SkyFire <admin@projectskyfire.org>
2014-01-01 14:33:39 -0600

    Missed ScriptDev2 Copyright updates. Sorry guys :)
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/EasternKingdoms/ZulAman/instance_zulaman.cpp
+++ src/server/scripts/EasternKingdoms/ZulAman/instance_zulaman.cpp
@@ -1,6 +1,6 @@
  /*
  * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2013 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2006-2014 ScriptDev2 <https://github.com/scriptdev2/scriptdev2/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
