1df50d1a973854a52537177970ee53832744e0c3
AlterEgo <admin@projectskyfire.org>
2017-07-09 11:02:57 -0500

    Update the revision hash parser.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -2,10 +2,11 @@
 #define __REVISION_H__
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
- #define VER_COMPANYNAME_STR        "Project SkyFire Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2011-2014 Project SkyFire & TrinityCore"
+ #define _BRANCH                    "@rev_branch@"
+ #define VER_COMPANYNAME_STR        "Project SkyFire"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2011-2017 Project SkyFire"
  #define VER_FILEVERSION            0,0,0
- #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
+ #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
 #endif // __REVISION_H__
